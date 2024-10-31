24d7fd4c209aca89e6cd9d9c7e811ff4d7cd1dab
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-10-31 16:32:39 -0600

    deps upgrades + fixes (#34)
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- apps/console/next.config.js
+++ apps/console/next.config.js
@@ -3,6 +3,5 @@ module.exports = {
   reactStrictMode: true,
   transpilePackages: ['@repo/dally', '@repo/ui', '@repo/codegen'],
   experimental: {
-    missingSuspenseWithCSRBailout: false,
   },
 }
