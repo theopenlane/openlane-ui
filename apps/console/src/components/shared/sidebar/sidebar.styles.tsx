27d4e56a6d923da3839e144fbd9914f217933017
Brian Wigginton <brianwigginton@gmail.com>
2024-12-05 16:26:43 -0600

    fix light and dark hovers

--- apps/console/src/components/shared/sidebar/sidebar.styles.tsx
+++ apps/console/src/components/shared/sidebar/sidebar.styles.tsx
@@ -8,7 +8,7 @@ const sidebarStyles = tv({
       'absolute gap-1 flex items-center justify-center bg-ziggurat-100 -right-[58px] w-[58px] h-[42px] bottom-0 cursor-pointer rounded-tr-lg border border-oxford-blue-200 border-b-0 text-3xl text-java-400 dark:bg-glaucous-950 dark:border-oxford-blue-200',
     expandNavIcon: 'rotate-180',
     sideNav:
-      'text-black bg-white dark:text-neutral-50 bg-glaucous-900 text-sm left-8 opacity-0 transition-all duration-300 group-hover:z-50 group-hover:ml-4 group-hover:rounded group-hover:bg-foreground group-hover:p-2 group-hover:opacity-100',
+      'text-black bg-white dark:text-neutral-50 dark:bg-glaucous-900 text-sm left-8 opacity-0 transition-all duration-300 group-hover:z-50 group-hover:ml-4 group-hover:rounded group-hover:bg-foreground group-hover:p-2 group-hover:opacity-100 group-hover:shadow-sm group-hover:border group-hover:dark:border-oxford-blue-700 group-hover:border-oxford-blue-200',
   },
   variants: {
     status: {
