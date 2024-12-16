b8bb126a2c2b16dd473c9f0eca62d4084483874b
Brian Wigginton <brianwigginton@gmail.com>
2024-12-16 15:45:25 -0600

    policies: use toast for errors

--- apps/console/src/app/(protected)/policies-and-procedures/policies/create/page.tsx
+++ apps/console/src/app/(protected)/policies-and-procedures/policies/create/page.tsx
@@ -1,35 +1,33 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { PageHeading } from '@repo/ui/page-heading'
-import {
-  PoliciesForm,
-  PolicyFormSchema,
-} from '@/components/pages/protected/policies/policies-form'
-import { NextPage } from 'next'
+import { PoliciesForm, PolicyFormSchema } from '@/components/pages/protected/policies/policies-form'
 import { useCreateInternalPolicyMutation } from '@repo/codegen/src/schema'
 import { useRouter } from 'next/navigation'
+import { useToast } from '@repo/ui/use-toast'
+import { useGqlError } from '@/hooks/useGqlError'
+import { NextPage } from 'next'
 
-type PageProps = {}
-
-const Page: NextPage<PageProps> = () => {
+const Page: NextPage = () => {
   const router = useRouter()
+  const { toast } = useToast()
 
-  const [{ fetching: isSubmitting }, createPolicy] =
-    useCreateInternalPolicyMutation()
+  const [result, createPolicy] = useCreateInternalPolicyMutation()
+  const { error } = result
+  const { errorMessages } = useGqlError(error)
 
-  // const onSubmit = (data: z.infer<typeof PolicyFormSchema>) => {
-  //   updatePolicy({
-  //     name: data.name,
-  //     description: data.description,
-  //     purposeAndScope: data.purposeAndScope,
-  //     policyType: data.policyType,
-  //     background: data.background,
-  //   })
-  // }
+  useEffect(() => {
+    if (!errorMessages?.length) return
+    toast({
+      title: `Error`,
+      description: 'We couldnt save the policy: ' + errorMessages.join('\n'),
+      variant: 'destructive',
+    })
+  }, [errorMessages])
 
   const onSubmit = async (policy: PolicyFormSchema) => {
-    const { data, error } = await createPolicy({
+    const { data } = await createPolicy({
       input: {
         name: policy.name,
         description: policy.description,
@@ -39,21 +37,10 @@ const Page: NextPage<PageProps> = () => {
         details: policy.details,
       },
     })
-    if (error) {
-      console.error('failed to save policy:', error)
-      return
-    }
 
-    if (!data?.createInternalPolicy) {
-      console.error('no internal policy returned:')
-      return
+    if (data?.createInternalPolicy?.internalPolicy?.id) {
+      router.push('/policies-and-procedures/policies/' + data?.createInternalPolicy.internalPolicy?.id || '')
     }
-
-    // load the policy page
-    router.push(
-      '/policies-and-procedures/policies/' +
-        data?.createInternalPolicy.internalPolicy.id,
-    )
   }
 
   return (
