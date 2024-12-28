33cc6a31b472ae009542ba9464b6e8b35bb1b19d
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-12-28 16:04:03 -0700

    env var example
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- apps/console/src/app/api/chat/route.tsx
+++ apps/console/src/app/api/chat/route.tsx
@@ -1,12 +1,12 @@
 import { bedrock } from "@ai-sdk/amazon-bedrock";
 import { createEdgeRuntimeAPI } from "@assistant-ui/react/edge";
-import { bedrockModelArn } from '@repo/dally/chat'
+import { bedrockModelArn, enableChat } from '@repo/dally/chat'
 
 const modelID = bedrockModelArn || "";
 
 let POST;
 
-if (process.env.AWS_REGION) {
+if (enableChat && process.env.AWS_REGION) {
     POST = createEdgeRuntimeAPI({
         model: bedrock(modelID),
     }).POST;
