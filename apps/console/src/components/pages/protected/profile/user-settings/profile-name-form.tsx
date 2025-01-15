b2b61369d7d62e348f74e713a07fd491e0d2d87f
papistacoding <bruno.papista@gmail.com>
2025-01-15 22:09:27 +0100

    profile complete

--- apps/console/src/components/pages/protected/profile/user-settings/profile-name-form.tsx
+++ apps/console/src/components/pages/protected/profile/user-settings/profile-name-form.tsx
@@ -1,5 +1,6 @@
 'use client'
-import { GetUserProfileQueryVariables, useGetUserProfileQuery, useUpdateUserMutation } from '@repo/codegen/src/schema'
+
+import { GetUserProfileQueryVariables, useGetUserProfileQuery, useUpdateUserMutation, useUpdateUserSettingMutation } from '@repo/codegen/src/schema'
 import { Input, InputRow } from '@repo/ui/input'
 import { Panel, PanelHeader } from '@repo/ui/panel'
 import { useSession } from 'next-auth/react'
@@ -10,12 +11,14 @@ import { z } from 'zod'
 import { Button } from '@repo/ui/button'
 import { useEffect, useState } from 'react'
 import { RESET_SUCCESS_STATE_MS } from '@/constants'
-import { AvatarUpload } from '@/components/shared/avatar-upload/avatar-upload'
 import { toast } from '@repo/ui/use-toast'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@repo/ui/tooltip'
+import { InfoIcon } from 'lucide-react'
 
 const ProfileNameForm = () => {
   const [isSuccess, setIsSuccess] = useState(false)
   const [{ fetching: isSubmitting }, updateUserName] = useUpdateUserMutation()
+
   const { data: sessionData } = useSession()
   const userId = sessionData?.user.userId
 
@@ -23,9 +26,7 @@ const ProfileNameForm = () => {
     userId: userId ?? '',
   }
 
-  const [{ data: userData }] = useGetUserProfileQuery({
-    variables,
-  })
+  const [{ data: userData }] = useGetUserProfileQuery({ variables })
 
   const formSchema = z.object({
     firstName: z.string().min(2, {
@@ -34,6 +35,9 @@ const ProfileNameForm = () => {
     lastName: z.string().min(2, {
       message: 'Last name must be at least 2 characters',
     }),
+    displayName: z.string().min(2, {
+      message: 'Display name must be at least 2 characters',
+    }),
   })
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -41,43 +45,26 @@ const ProfileNameForm = () => {
     defaultValues: {
       firstName: userData?.user.firstName || '',
       lastName: userData?.user.lastName || '',
+      displayName: userData?.user.displayName || '',
     },
   })
 
-  const updateName = async ({ firstName, lastName }: { firstName: string; lastName: string }) => {
-    await updateUserName({
-      updateUserId: userId,
-      input: {
-        firstName: firstName,
-        lastName: lastName,
-      },
-    })
-    setIsSuccess(true)
-  }
-
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
-    await updateName({ firstName: data.firstName, lastName: data.lastName })
-  }
-
-  const handleUploadAvatar = async (file: File) => {
-    if (!userId) return
-
     try {
       await updateUserName({
         updateUserId: userId,
-        input: {},
-        avatarFile: file,
+        input: {
+          firstName: data.firstName,
+          lastName: data.lastName,
+          displayName: data.displayName,
+        },
       })
-
       setIsSuccess(true)
-      toast({
-        title: 'Avatar updated successfully',
-        variant: 'success',
-      })
+      toast({ title: 'Profile updated successfully!', variant: 'success' })
     } catch (error) {
-      console.error('file upload error')
+      console.error('Failed to update profile:', error)
       toast({
-        title: 'Failed to update avatar',
+        title: 'An error occurred while updating your profile.',
         variant: 'destructive',
       })
     }
@@ -88,6 +75,7 @@ const ProfileNameForm = () => {
       form.reset({
         firstName: userData.user.firstName ?? '',
         lastName: userData.user.lastName ?? '',
+        displayName: userData?.user.displayName ?? '',
       })
     }
   }, [userData])
@@ -102,51 +90,87 @@ const ProfileNameForm = () => {
   }, [isSuccess])
 
   return (
-    <>
-      <Panel>
-        <PanelHeader heading="Your name" noBorder />
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <InputRow>
-              <FormField
-                control={form.control}
-                name="firstName"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>First name</FormLabel>
-                    <FormControl>
-                      <Input variant="medium" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="lastName"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Last name</FormLabel>
-                    <FormControl>
-                      <Input variant="medium" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button variant={isSuccess ? 'success' : 'filled'} type="submit" loading={isSubmitting}>
-                {isSubmitting ? 'Saving' : isSuccess ? 'Saved' : 'Save'}
-              </Button>
-            </InputRow>
-          </form>
-        </Form>
-      </Panel>
-      <AvatarUpload
-        fallbackString={sessionData?.user?.name?.substring(0, 2)}
-        uploadCallback={handleUploadAvatar || 'N/A'}
-        placeholderImage={userData?.user.avatarFile?.presignedURL || sessionData?.user?.image}
-      />
-    </>
+    <Panel>
+      <PanelHeader heading="Your Profile" noBorder />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <InputRow>
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First name</FormLabel>
+                  <TooltipProvider disableHoverableContent={true}>
+                    <Tooltip>
+                      <TooltipTrigger type="button">
+                        <InfoIcon size={14} className="mx-1" />
+                      </TooltipTrigger>
+                      <TooltipContent side="right">
+                        <p>Your official name as recognized on legal documents like IDs, tax forms, or contracts.</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                  <FormControl>
+                    <Input variant="medium" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last name</FormLabel>
+                  <TooltipProvider disableHoverableContent={true}>
+                    <Tooltip>
+                      <TooltipTrigger type="button">
+                        <InfoIcon size={14} className="mx-1" />
+                      </TooltipTrigger>
+                      <TooltipContent side="right">
+                        <p>Your official last name as recognized on legal documents like IDs, tax forms, or contracts.</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                  <FormControl>
+                    <Input variant="medium" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="displayName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Display name</FormLabel>
+                  <TooltipProvider disableHoverableContent={true}>
+                    <Tooltip>
+                      <TooltipTrigger type="button">
+                        <InfoIcon size={14} className="mx-1" />
+                      </TooltipTrigger>
+                      <TooltipContent side="right">
+                        <p>Your official name as recognized on legal documents like IDs, tax forms, or contracts.</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                  <FormControl>
+                    <Input variant="medium" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button variant={isSuccess ? 'success' : 'filled'} type="submit" loading={isSubmitting}>
+              {isSubmitting ? 'Saving' : isSuccess ? 'Saved' : 'Save'}
+            </Button>
+          </InputRow>
+        </form>
+      </Form>
+    </Panel>
   )
 }
 
---
+++ apps/console/src/components/pages/protected/profile/user-settings/profile-page.tsx
@@ -0,0 +1,62 @@
+'use client'
+import React, { Suspense } from 'react'
+import { ProfileNameForm } from './profile-name-form'
+import { AvatarUpload } from '@/components/shared/avatar-upload/avatar-upload'
+import { useSession } from 'next-auth/react'
+import { GetUserProfileQueryVariables, useGetUserProfileQuery, useUpdateUserMutation } from '@repo/codegen/src/schema'
+import { toast } from '@repo/ui/use-toast'
+import DefaultOrgForm from './default-org-form'
+import { Loader } from 'lucide-react'
+
+const ProfilePage = () => {
+  const { data: sessionData } = useSession()
+  const userId = sessionData?.user.userId
+
+  const variables: GetUserProfileQueryVariables = {
+    userId: userId ?? '',
+  }
+
+  const [{ data: userData }] = useGetUserProfileQuery({
+    variables,
+  })
+
+  const [{ fetching: isSubmitting }, updateUser] = useUpdateUserMutation()
+
+  const handleUploadAvatar = async (file: File) => {
+    if (!userId) return
+
+    try {
+      await updateUser({
+        updateUserId: userId,
+        input: {},
+        avatarFile: file,
+      })
+
+      toast({
+        title: 'Avatar updated successfully',
+        variant: 'success',
+      })
+    } catch (error) {
+      console.error('file upload error')
+      toast({
+        title: 'Failed to update avatar',
+        variant: 'destructive',
+      })
+    }
+  }
+  return (
+    <>
+      <ProfileNameForm />
+      <AvatarUpload
+        fallbackString={sessionData?.user?.name?.substring(0, 2)}
+        uploadCallback={handleUploadAvatar || 'N/A'}
+        placeholderImage={userData?.user.avatarFile?.presignedURL || sessionData?.user?.image}
+      />
+      <Suspense fallback={<Loader />}>
+        <DefaultOrgForm />
+      </Suspense>
+    </>
+  )
+}
+
+export default ProfilePage
