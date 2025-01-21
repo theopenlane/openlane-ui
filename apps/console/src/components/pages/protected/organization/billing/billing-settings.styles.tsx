d3eab055c5cb7f8e935bc1f4cf1311c3ca92ae9f
papistacoding <bruno.papista@gmail.com>
2025-01-21 16:13:14 +0100

    smaller gap on billing, adding different border

--- apps/console/src/components/pages/protected/organization/billing/billing-settings.styles.tsx
+++ apps/console/src/components/pages/protected/organization/billing/billing-settings.styles.tsx
@@ -3,7 +3,7 @@ import { tv, type VariantProps } from 'tailwind-variants'
 const billingSettingsStyles = tv({
   slots: {
     panel: 'p-6 w-2/3',
-    section: 'flex justify-between items-start py-6 border-b border-gray-300 last:border-b-0',
+    section: 'flex justify-between items-start py-6  border-b  last:border-b-0',
     sectionTitle: 'text-xl font-medium text-text-header w-1/5',
     sectionContent: 'flex justify-between w-full gap-4',
     text: 'text-sm mb-2',
