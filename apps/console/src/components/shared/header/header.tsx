3c05d17503d61bf5087e8593eb103e6fdd878d98
Bruno Papista <78353799+papistacoding@users.noreply.github.com>
2025-01-21 16:57:19 +0100

    reposition shrink toggler (#157)
    * reposition shrink toggler
    
    * cleaning code

--- apps/console/src/components/shared/header/header.tsx
+++ apps/console/src/components/shared/header/header.tsx
@@ -1,18 +1,40 @@
+'use client'
 import Link from 'next/link'
 import { headerStyles } from './header.styles'
 import { UserMenu } from '@/components/shared/user-menu/user-menu'
 import { OrganizationSelector } from '@/components/shared/organization-selector/organization-selector'
 import { BreadcrumbNavigation } from '@/components/shared/breadcrumb-nav/breadcrumb'
 import { GlobalSearch } from '@/components/shared/search/search'
-// import { Notifications } from '../notifications/notifications'
+import { sidebarStyles } from '../sidebar/sidebar.styles'
+import { useSidebar } from '@/hooks/useSidebar'
+import { useState } from 'react'
+import { PanelLeft } from 'lucide-react'
 
 export default function Header() {
+  const { isOpen, toggle } = useSidebar()
+  const [status, setStatus] = useState(false)
+
   const { header, nav, mobileSidebar, userNav } = headerStyles()
+  const { expandNav } = sidebarStyles({
+    status,
+    isOpen,
+  })
+
+  const handleToggle = () => {
+    setStatus(true)
+    toggle()
+    setTimeout(() => setStatus(false), 500)
+  }
+
   return (
     <div className={header()}>
       <nav className={nav()}>
         <div className="flex justify-start items-center">
           <OrganizationSelector />
+
+          <div className={expandNav({ isOpen: !isOpen })} onClick={handleToggle}>
+            <PanelLeft height={16} width={16} />
+          </div>
 
           <div className={mobileSidebar()}>
             <>MobileSidebar</>
@@ -27,7 +49,6 @@ export default function Header() {
           <GlobalSearch />
           <Link href="mailto:support@theopenlane.io">Feedback</Link>
           <Link href="https://docs.theopenlane.io">Docs</Link>
-          {/* <Notifications /> */}
           <UserMenu />
         </div>
       </nav>
