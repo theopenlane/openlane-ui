19632fe6bf679374a730419fb852950a4d091ef5
papistacoding <bruno.papista@gmail.com>
2025-01-22 20:57:52 +0100

    progress

--- apps/console/src/components/pages/protected/profile/user-settings/profile-page.tsx
+++ apps/console/src/components/pages/protected/profile/user-settings/profile-page.tsx
@@ -1,12 +1,14 @@
 'use client'
-import React, { Suspense } from 'react'
+import React, { Suspense, useState } from 'react'
 import { ProfileNameForm } from './profile-name-form'
 import { AvatarUpload } from '@/components/shared/avatar-upload/avatar-upload'
 import { useSession } from 'next-auth/react'
-import { GetUserProfileQueryVariables, useGetUserProfileQuery, useUpdateUserMutation } from '@repo/codegen/src/schema'
+import { GetUserProfileQueryVariables, useGetUserProfileQuery, useGetUserTfaSettingsQuery, useUpdateTfaSettingMutation, useUpdateUserMutation } from '@repo/codegen/src/schema'
 import { toast } from '@repo/ui/use-toast'
 import DefaultOrgForm from './default-org-form'
 import { Loader } from 'lucide-react'
+import { Checkbox } from '@repo/ui/checkbox'
+import { Label } from '@repo/ui/label'
 
 const ProfilePage = () => {
   const { data: sessionData } = useSession()
@@ -22,6 +24,9 @@ const ProfilePage = () => {
 
   const [{ fetching: isSubmitting }, updateUser] = useUpdateUserMutation()
 
+  const [{ data: tfaData }] = useGetUserTfaSettingsQuery({ variables })
+  const [{ fetching: isTfaSubmitting }, updateTfaSetting] = useUpdateTfaSettingMutation()
+  console.log('tfaData', tfaData)
   const handleUploadAvatar = async (file: File) => {
     if (!userId) return
 
@@ -44,6 +49,28 @@ const ProfilePage = () => {
       })
     }
   }
+
+  const handleTfaChange = async (checked: boolean) => {
+    try {
+      await updateTfaSetting({
+        input: {
+          totpAllowed: checked,
+        },
+      })
+
+      toast({
+        title: `Two-factor authentication ${checked ? 'enabled' : 'disabled'} successfully`,
+        variant: 'success',
+      })
+    } catch (error) {
+      console.error('Error updating TFA setting:', error)
+      toast({
+        title: 'Failed to update TFA setting',
+        variant: 'destructive',
+      })
+    }
+  }
+
   return (
     <>
       <ProfileNameForm />
@@ -52,6 +79,10 @@ const ProfilePage = () => {
         uploadCallback={handleUploadAvatar || 'N/A'}
         placeholderImage={userData?.user.avatarFile?.presignedURL || sessionData?.user?.image}
       />
+      <div className="tfa-settings">
+        <Checkbox checked={tfaData?.user?.tfaSettings?.totpAllowed ?? false} disabled={isTfaSubmitting} onCheckedChange={handleTfaChange}></Checkbox>
+        <Label>Enable Two-Factor Authentication</Label>
+      </div>
       <Suspense fallback={<Loader />}>
         <DefaultOrgForm />
       </Suspense>
