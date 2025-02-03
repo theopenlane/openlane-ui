8632d6c70929ec3dd11983700595b487b6005b31
papistacoding <bruno.papista@gmail.com>
2025-02-03 19:23:33 +0100

    styling

--- apps/console/src/components/shared/avatar-list/avatar-list.tsx
+++ apps/console/src/components/shared/avatar-list/avatar-list.tsx
@@ -51,9 +51,9 @@ const AvatarList = ({ data, max = 10 }: AvatarListProps) => {
             </div>
           </PopoverTrigger>
 
-          <PopoverContent onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="max-h-80 overflow-y-auto w-60 p-2 bg-background-secondary shadow-md rounded-md border">
-            {hiddenAvatars.map(({ id, firstName, lastName, fallback }) => (
-              <div key={id} className="flex items-center gap-2 p-2 rounded-md">
+          <PopoverContent onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="max-h-80 overflow-y-auto w-60  bg-background-secondary shadow-md rounded-md border">
+            {visibleAvatars.map(({ id, firstName, lastName, fallback }, index) => (
+              <div key={id} className={`flex items-center gap-2 p-2 ${index !== visibleAvatars.length - 1 ? 'border-b' : ''}`}>
                 <Avatar className="w-5 h-5">
                   <AvatarFallback>{fallback}</AvatarFallback>
                 </Avatar>
