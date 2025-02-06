301b8c9495df2b0adeabe900592bf45af21fa4f4
Brian Wigginton <brianwigginton@gmail.com>
2025-02-06 10:52:26 -0600

    fix: editable textarea focus

--- packages/ui/src/textarea.tsx
+++ packages/ui/src/textarea.tsx
@@ -21,8 +21,14 @@ Textarea.displayName = 'Textarea'
 type EditableTextareaProps = React.ComponentProps<'textarea'>
 
 const EditableTextarea = React.forwardRef<HTMLTextAreaElement, EditableTextareaProps>(({ className, ...props }, ref) => {
-  const textarea = React.useRef()
+  const textareaRef = React.useRef<HTMLTextAreaElement>(null)
   const [editing, setEditing] = React.useState<boolean>(false)
+
+  React.useEffect(() => {
+    if (editing && textareaRef?.current) {
+      textareaRef.current.focus()
+    }
+  }, [editing])
 
   if (editing) {
     return (
@@ -34,7 +40,7 @@ const EditableTextarea = React.forwardRef<HTMLTextAreaElement, EditableTextareaP
           'flex min-h-[80px] w-full focus:rounded-md border border-oxford-blue-200 bg-white dark:bg-glaucous-900 px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
           className,
         )}
-        ref={ref}
+        ref={textareaRef}
         {...props}
       />
     )
