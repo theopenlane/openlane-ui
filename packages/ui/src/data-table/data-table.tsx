94377652820893b6b5aef87c6698155e258706a2
papistacoding <bruno.papista@gmail.com>
2025-02-14 13:38:20 +0100

    table row hover

--- packages/ui/src/data-table/data-table.tsx
+++ packages/ui/src/data-table/data-table.tsx
@@ -127,7 +127,13 @@ export function DataTable<TData, TValue>({
         <TableBody>
           {table.getRowModel().rows?.length ? (
             table.getRowModel().rows.map((row) => (
-              <TableRow onClick={() => onRowClick?.(row.original)} key={row.id} data-state={row.getIsSelected() && 'selected'}>
+              <TableRow
+                onClick={() => onRowClick?.(row.original)}
+                className={`${onRowClick ? 'hover:bg-table-row-bg-hover cursor-pointer' : ''}`}
+                key={row.id}
+                data-state={row.getIsSelected() && 'selected'}
+              >
+                {' '}
                 {row.getVisibleCells().map((cell) => (
                   // @ts-ignore
                   <TableCell key={cell.id} className={cell.column.columnDef.meta?.className || ''}>
