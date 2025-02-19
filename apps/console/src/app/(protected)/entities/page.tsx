371d3cc1a81d180e3bdc6235e54344a43c81371c
Bruno Papista <78353799+papistacoding@users.noreply.github.com>
2025-02-19 01:07:44 +0100

    Update apps/console/src/app/(protected)/entities/page.tsx
    Co-authored-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    Signed-off-by: Bruno Papista <78353799+papistacoding@users.noreply.github.com>

--- apps/console/src/app/(protected)/entities/page.tsx
+++ apps/console/src/app/(protected)/entities/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { PageHeading } from '@repo/ui/page-heading'
 
 const Page: React.FC = () => {
-  return <PageHeading heading="Vendors" />
+  return <PageHeading heading="Entities" />
 }
 
 export default Page
