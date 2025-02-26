b228c245ac667c750e27a68824cde88c6ef3adec
Brian Wigginton <brianwigginton@gmail.com>
2025-02-26 17:31:32 -0600

    new UI for policy view

--- apps/console/src/components/pages/protected/policies/policy-page.tsx
+++ apps/console/src/components/pages/protected/policies/policy-page.tsx
@@ -1,13 +1,16 @@
 import React, { useState, useCallback, useEffect } from 'react'
+import { Button } from '@repo/ui/button'
 import { PageHeading } from '@repo/ui/page-heading'
-import { PolicyInfoBar } from '@/components/pages/protected/policies/policy-info-bar'
 import { PolicySidebar } from '@/components/pages/protected/policies/policy-sidebar'
 import dynamic from 'next/dynamic'
 import { TElement } from '@udecode/plate-common'
 import { useGetInternalPolicyDetailsByIdQuery, useUpdateInternalPolicyMutation, useDeleteInternalPolicyMutation } from '@repo/codegen/src/schema'
-import type { InternalPolicyByIdFragment, InternalPolicyUpdateFieldsFragment } from '@repo/codegen/src/schema'
-const PlateEditor = dynamic(() => import('@/components/shared/editor/plate'), { ssr: false })
+import { useRouter } from 'next/navigation'
 import { z } from 'zod'
+import { Pencil } from 'lucide-react'
+
+import type { InternalPolicyByIdFragment } from '@repo/codegen/src/schema'
+const PlateEditor = dynamic(() => import('@/components/shared/editor/plate'), { ssr: false })
 
 export type EditableField = 'name' | 'description' | 'background' | 'purposeAndScope'
 
@@ -28,12 +31,19 @@ type PolicyPageProps = {
   policyId: string
 }
 
+// Sorts an array of objects by a specified key by moving null values to the end
+function sortEmptiesToEndByKey<T extends Record<string, any>>(arr: T[], key: keyof T): T[] {
+  return arr.filter((item) => item[key]).concat(arr.filter((item) => !item[key]))
+}
+
 export function PolicyPage({ policyId }: PolicyPageProps) {
+  const router = useRouter()
+
   const [, updatePolicy] = useUpdateInternalPolicyMutation()
   const [{ data }] = useGetInternalPolicyDetailsByIdQuery({ variables: { internalPolicyId: policyId } })
   const [{ data: deleteData }] = useDeleteInternalPolicyMutation()
   const [document, setDocument] = useState([] as TElement[])
-  const [policy, setPolicy] = useState({} as InternalPolicyUpdateFieldsFragment)
+  const [policy, setPolicy] = useState({} as InternalPolicyByIdFragment)
 
   useEffect(() => {
     if (!data?.internalPolicy) return
@@ -89,19 +99,37 @@ export function PolicyPage({ policyId }: PolicyPageProps) {
 
   if (!data?.internalPolicy) return <></>
 
+  const descriptions = sortEmptiesToEndByKey(
+    [
+      { label: 'Purpose and Scope', value: policy.purposeAndScope },
+      { label: 'Description', value: policy.description },
+      { label: 'Background', value: policy.background },
+    ],
+    'value',
+  )
+
+  const policyName = policy.displayID ? `${policy.displayID} - ${policy.name}` : policy.name
+
   return (
     <>
-      <PageHeading className="grow" eyebrow="Policies & Procedures" heading={policy.name} editable onChange={onNameChange} />
+      <PageHeading className="grow" eyebrow="Policies & Procedures" heading={policyName} />
 
-      <PolicyInfoBar policy={policy} handleSave={saveCurrentPolicy} />
-
-      <div className="flex flex-col md:flex-row-reverse gap-5 w-full">
-        <PolicySidebar policy={policy} onFieldChange={setField} />
-
-        <div className="w-full">
-          <div>
-            <PlateEditor content={document} onChange={onDocumentChange} />
+      <div className="flex flex-row gap-5">
+        <div className="grow">
+          <div className="flex flex-col divide-y divide-border mb-10">
+            {descriptions.map((desc) => (
+              <div className="flex flex-row py-5">
+                <div className="w-40 min-w-40 font-bold">{desc.label}</div>
+                <div>{desc.value || <span className="italic">None</span>}</div>
+              </div>
+            ))}
           </div>
+
+          <PlateEditor content={document} onChange={onDocumentChange} />
+        </div>
+
+        <div className="w-[600px]">
+          <PolicySidebar policy={policy} />
         </div>
       </div>
     </>
--- apps/console/src/components/pages/protected/policies/policy-sidebar.tsx
+++ apps/console/src/components/pages/protected/policies/policy-sidebar.tsx
@@ -1,38 +1,52 @@
 'use client'
 
 import React from 'react'
-import { Panel, PanelHeader } from '@repo/ui/panel'
-import { Button } from '@repo/ui/button'
-import { EditableTextarea } from '@repo/ui/textarea'
-import { EditableField } from './policy-page'
 import { InternalPolicyByIdFragment } from '@repo/codegen/src/schema'
+import { CircleUser, UserRoundCheck, Binoculars, FileStack, ScrollText, Tag, CalendarCheck2, UserRoundPen, CalendarClock } from 'lucide-react'
+import { Badge } from '@repo/ui/badge'
+import { MetaPanel, formatTime } from '@/components/shared/meta-panel/meta-panel'
 
 type PolicySidebarProps = {
   policy: InternalPolicyByIdFragment
-  onFieldChange: (field: EditableField, value: string) => void
 }
 
-export const PolicySidebar: React.FC<PolicySidebarProps> = function ({ policy, onFieldChange }) {
-  return (
-    <div className="flex flex-col gap-5 w-full">
-      <Panel className="p-0 border-0 gap-0">
-        <PanelHeader heading="Description" className="p-4 text-base" noBorder />
+export const PolicySidebar: React.FC<PolicySidebarProps> = function ({ policy }) {
+  const sidebarItems = {
+    ownership: [
+      { icon: CircleUser, label: 'Owner', value: 'owner' },
+      { icon: UserRoundCheck, label: 'Approver', value: 'approver' },
+    ],
+    status: [
+      { icon: Binoculars, label: 'Status', value: policy.status },
+      { icon: FileStack, label: 'Version', value: policy.version },
+      { icon: ScrollText, label: 'Type', value: policy.policyType },
+      {
+        icon: Tag,
+        label: 'Tags',
+        value: policy.tags?.length ? (
+          policy.tags.map((t) => (
+            <Badge variant="outline" className="mr-1">
+              {t}
+            </Badge>
+          ))
+        ) : (
+          <span className="italic">none</span>
+        ),
+      },
+    ],
+    creation: [
+      { icon: UserRoundPen, label: 'Created By', value: policy.createdBy },
+      { icon: CalendarCheck2, label: 'Created At', value: formatTime(policy.createdAt) },
+      { icon: UserRoundCheck, label: 'Updated By', value: policy.updatedBy },
+      { icon: CalendarClock, label: 'Updated At', value: formatTime(policy.updatedAt) },
+    ],
+  }
 
-        <div className="divide-y divide-oxford-blue-100 dark:divide-oxford-blue-900 *:px-4 *:py-2">
-          <div>
-            <h3 className="text-oxford-blue-500 text-sm mb-1">Description</h3>
-            <EditableTextarea rows={7} onChange={(e) => onFieldChange('description', e.target.value)} value={policy.description ?? ''} placeholder="provide a description" />
-          </div>
-          <div>
-            <h3 className="text-oxford-blue-500 text-sm mb-1">Background</h3>
-            <EditableTextarea rows={7} onChange={(e) => onFieldChange('background', e.target.value)} value={policy.background ?? ''} placeholder="provide a background" />
-          </div>
-          <div>
-            <h3 className="text-oxford-blue-500 text-sm mb-1">Purpose and Scope</h3>
-            <EditableTextarea rows={7} onChange={(e) => onFieldChange('purposeAndScope', e.target.value)} value={policy.purposeAndScope ?? ''} placeholder="provide a purpose and scope" />
-          </div>
-        </div>
-      </Panel>
+  return (
+    <div className="flex flex-col gap-5">
+      <MetaPanel entries={sidebarItems.ownership} />
+      <MetaPanel entries={sidebarItems.status} />
+      <MetaPanel entries={sidebarItems.creation} />
     </div>
   )
 }
