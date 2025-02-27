24dfb350a830d206d96d0354e05c3ad8fcfcf044
Brian Wigginton <brianwigginton@gmail.com>
2025-02-26 21:37:29 -0600

    add document descriptions component

---
+++ apps/console/src/components/shared/document-descriptions/document-descriptions.tsx
@@ -0,0 +1,25 @@
+import { useMemo } from 'react'
+
+// Sorts an array of objects by a specified key by moving null values to the end
+function sortEmptiesToEndByKey<T extends Record<string, any>>(arr: T[], key: keyof T): T[] {
+  return arr.filter((item) => item[key]).concat(arr.filter((item) => !item[key]))
+}
+
+export function DocumentDescriptions({ descriptions }: { descriptions: { label: string; value?: string | null }[] }) {
+  if (!descriptions) return null
+
+  const descriptionsEmptyLast = useMemo(() => {
+    return sortEmptiesToEndByKey(descriptions, 'value')
+  }, [descriptions])
+
+  return (
+    <div className="flex flex-col divide-y divide-border mb-10">
+      {descriptionsEmptyLast.map((desc) => (
+        <div key={desc.label} className="flex flex-row py-5">
+          <div className="w-40 min-w-40 font-bold">{desc.label}</div>
+          <div>{desc.value || <span className="italic">None</span>}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
