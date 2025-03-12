40bbac40040abef0269a6c27c3cf7ab1bc0529ce
Mateo Kantoci <info@mateo-it.com>
2025-03-12 16:56:05 +0100

    evidence-phase-2: fixed task comments style

--- apps/console/src/components/shared/comments/CommentList.tsx
+++ apps/console/src/components/shared/comments/CommentList.tsx
@@ -12,17 +12,22 @@ const CommentList: React.FC<TProps> = (props: TProps) => {
     <>
       {props.comments.map((item, index) => (
         <div className="w-full p-2" key={`${item.userName}-${index}`}>
-          <div className="flex items-center space-x-3">
-            <Avatar variant="medium" className="relative flex shrink-0 overflow-hidden rounded-full p-0 h-10 w-10 mr-2">
-              {item?.avatarUrl && <AvatarImage src={item.avatarUrl} />}
-              <AvatarFallback>{item.userName?.substring(0, 2)}</AvatarFallback>
-            </Avatar>
-            <p className="font-semibold">{item.userName}</p>
-            <p className="text-sm">{format(new Date(item.createdAt), 'MMMM dd, yyyy - hh:mm a')}</p>
-          </div>
-
-          <div className="mt-1 ml-14 border rounded-lg p-3">
-            <p>{item.comment}</p>
+          <div className="flex">
+            <div className="w-1/10 flex justify-center">
+              <Avatar variant="medium" className="relative flex shrink-0 overflow-hidden rounded-full p-0 h-10 w-10">
+                {item?.avatarUrl && <AvatarImage src={item.avatarUrl} />}
+                <AvatarFallback>{item.userName?.substring(0, 2)}</AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="w-9/10 pl-3 w-full">
+              <div className="flex items-center gap-2">
+                <p className="font-semibold">{item.userName}</p>
+                <p className="text-sm">{format(new Date(item.createdAt), 'MMMM dd, yyyy - hh:mm a')}</p>
+              </div>
+              <div className="border rounded-lg p-3 mt-2 w-full">
+                <p>{item.comment}</p>
+              </div>
+            </div>
           </div>
         </div>
       ))}
