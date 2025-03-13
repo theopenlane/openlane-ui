74e96756489f53fb5ec15d8ad9692463e3f9d778
papistacoding <bruno.papista@gmail.com>
2025-03-13 19:16:59 +0100

    textarea and signup

--- packages/ui/src/textarea.tsx
+++ packages/ui/src/textarea.tsx
@@ -1,18 +1,12 @@
 import * as React from 'react'
-
 import { cn } from '@repo/ui/lib/utils'
 
+const textareaStyles = cn(
+  'flex min-h-[80px] w-full rounded-md border border-neutral-300 dark:border-brand bg-input-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+)
+
 const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'textarea'>>(({ className, ...props }, ref) => {
-  return (
-    <textarea
-      className={cn(
-        'flex min-h-[80px] w-full rounded-md border border-border bg-background-secondary px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-        className,
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
+  return <textarea className={cn(textareaStyles, className)} ref={ref} {...props} />
 })
 
 Textarea.displayName = 'Textarea'
@@ -30,32 +24,16 @@ const EditableTextarea = React.forwardRef<HTMLTextAreaElement, EditableTextareaP
   }, [editing])
 
   if (editing) {
-    return (
-      <textarea
-        onBlur={(e) => {
-          setEditing(false)
-        }}
-        className={cn(
-          'flex min-h-[80px] w-full focus:rounded-md border border-oxford-blue-200 bg-white dark:bg-glaucous-900 px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-          className,
-        )}
-        ref={textareaRef}
-        {...props}
-      />
-    )
+    return <textarea onBlur={() => setEditing(false)} className={cn(textareaStyles, className)} ref={textareaRef} {...props} />
   }
 
   return (
-    <p
-      onClick={(e) => {
-        setEditing(true)
-      }}
-      className={`hover:background hover:cursor-pointer whitespace-pre-line ${!props.value && '!text-neutral-400'}`}
-    >
+    <p onClick={() => setEditing(true)} className={`hover:bg-muted hover:cursor-pointer whitespace-pre-line ${!props.value && '!text-neutral-400'}`}>
       {props.value || props.placeholder}
     </p>
   )
 })
+
 EditableTextarea.displayName = 'EditableTextarea'
 
 export { Textarea, EditableTextarea }
