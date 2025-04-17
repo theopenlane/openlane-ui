3732dc0c393813eb77c9926f399ce10cdbfbef89
Lanre Adelowo <yo@lanre.wtf>
2025-04-17 03:17:57 +0100

    implement passkey provider

--- apps/console/src/lib/auth/auth.ts
+++ apps/console/src/lib/auth/auth.ts
@@ -12,6 +12,7 @@ import { cookies, type UnsafeUnwrappedCookies } from 'next/headers'
 import { sessionCookieName, allowedLoginDomains } from '@repo/dally/auth'
 import { fetchNewAccessToken } from './utils/refresh-token'
 import { getDashboardData } from '@/app/api/getDashboardData/route'
+import { passKeyProvider } from './providers/passkey'
 
 export const config = {
   pages: {
@@ -34,7 +35,7 @@ export const config = {
       checks: isDevelopment ? ['none'] : undefined,
     }),
     credentialsProvider,
-    // passKeyProvider,
+    passKeyProvider,
   ],
   events: {
     async signOut() {
