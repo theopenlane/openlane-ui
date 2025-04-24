f2dabaf4872fa32da29a9cdfe9684dc19d19f090
papistacoding <bruno.papista@gmail.com>
2025-04-24 12:54:50 +0200

    fixing risks

--- packages/ui/src/tabs/tabs.styles.tsx
+++ packages/ui/src/tabs/tabs.styles.tsx
@@ -10,8 +10,12 @@ const tabsStyles = tv({
     variant: {
       underline: {
         tabsList: 'rounded-lg p-1 h-9 items-center justify-center',
-        tabsTrigger:
-          'flex-1 rounded-t-xl items-center justify-center whitespace-nowrap px-3 py-2 font-sans text-text-dark border-b border-border-dark ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-teal-400 data-[state=active]:!bg-opacity-20 data-[state=active]:border-b-2',
+        tabsTrigger: [
+          'flex-1 rounded-t-xl items-center justify-center whitespace-nowrap px-3 py-2 font-sans ',
+          'transition-all ring-offset-background focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50',
+          'shadow-[inset_0_-1px_0_0_var(--color-border)]', // default: 1px border-style line
+          'data-[state=active]:shadow-[inset_0_-2px_0_0_var(--color-brand)]', // active: 2px brand bottom line
+        ].join(' '),
       },
       solid: {
         tabsList: 'rounded-md bg-panel p-[10px] items-start',
