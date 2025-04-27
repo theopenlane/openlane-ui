9fd3f3e92c0518b533bb3f0e664c156cb615a9cb
papistacoding <bruno.papista@gmail.com>
2025-04-27 13:51:09 +0200

    fixing auth pages style

--- apps/console/src/components/layouts/auth/auth.styles.tsx
+++ apps/console/src/components/layouts/auth/auth.styles.tsx
@@ -2,7 +2,7 @@ import { tv, type VariantProps } from 'tailwind-variants'
 
 const authStyles = tv({
   slots: {
-    base: 'relative bg-glaucous-900 flex flex-col h-full w-full items-center justify-center',
+    base: 'relative bg-glaucous-900 flex flex-col h-full w-full items-center justify-center min-h-screen',
     closeButton: 'absolute top-2 right-2 md:top-10 md:right-10 text-white',
     closeButtonIcon: 'h-8 w-8 z-20',
   },
