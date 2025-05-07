ec6629d6582192f9b51ae58b8357709f2c44a9a6
Kelsey Waters <149399144+LemonAid711@users.noreply.github.com>
2025-05-07 13:53:20 -0500

    signup-text-update

--- apps/console/src/app/(auth)/signup/page.tsx
+++ apps/console/src/app/(auth)/signup/page.tsx
@@ -11,8 +11,8 @@ const AuthLogin: React.FC = () => {
         <div className="flex h-full w-full min-h-screen gap-4 sm:gap-8 md:gap-12 lg:gap-20 xl:gap-60 justify-center lg:justify-start">
           <AuthMarketingPanel>
             <h2 className="text-4xl font-normal">
-              Compliance you
-              <br /> can verify, not <br /> just report.
+              Because compliance
+              <br /> isn't just <br /> a checkbox.
             </h2>
           </AuthMarketingPanel>{' '}
           <SignupPage />
