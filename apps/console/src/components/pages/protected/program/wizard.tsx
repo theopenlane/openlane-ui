debf82080f2223dcbb9371cf4dd4bc2a64504bb9
Lanre Adelowo <yo@lanre.wtf>
2025-05-12 20:07:29 +0100

    add check

--- apps/console/src/components/pages/protected/program/wizard.tsx
+++ apps/console/src/components/pages/protected/program/wizard.tsx
@@ -137,7 +137,7 @@ const ProgramWizard = ({ onSuccess, requestClose, blockClose }: ProgramWizardPro
       return false
     }
 
-    if (formData.programType === 'framework' && (!formData.framework || formData.framework.trim() === '')) {
+    if (formData.programType === 'framework' && (!formData.framework || formData.framework.trim() === '') && (!formData.standardID || formData?.standardID?.trim() === '')) {
       return false
     }
     return true
