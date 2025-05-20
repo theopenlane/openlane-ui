27c8b780cf7dc9de8d07d9bbefa3c667f3be498a
Mateo Kantoci <info@mateo-it.com>
2025-05-21 00:50:42 +0200

    download-file-fix: added native URL download

--- apps/console/src/components/shared/lib/export.ts
+++ apps/console/src/components/shared/lib/export.ts
@@ -1,18 +1,11 @@
 export const fileDownload = async (presignedURL: string, fileName: string, errorNotification: (args: { title: string; variant: 'destructive' | 'default' }) => void) => {
   try {
-    const response = await fetch(presignedURL, {
-      mode: 'cors',
-      credentials: 'omit',
-    })
-    const blob = await response.blob()
-    const url = window.URL.createObjectURL(blob)
     const link = document.createElement('a')
-    link.href = url
-    link.download = fileName
+    link.href = presignedURL
+    link.setAttribute('download', fileName)
     document.body.appendChild(link)
     link.click()
     document.body.removeChild(link)
-    window.URL.revokeObjectURL(url)
   } catch (error) {
     errorNotification({
       title: 'An error occurred while downloading file. Please try again.',
