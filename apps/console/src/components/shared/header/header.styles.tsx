e9f96e6313e62838dd928b3518a6f71850377741
papistacoding <bruno.papista@gmail.com>
2025-06-04 22:23:31 +0200

    fixing gap

--- apps/console/src/components/shared/header/header.styles.tsx
+++ apps/console/src/components/shared/header/header.styles.tsx
@@ -5,7 +5,7 @@ const headerStyles = tv({
     header: 'sticky left-0 right-0 top-0 z-20 py-4 h-16 border-b border-border bg-background-secondary',
     nav: 'flex h-[30px] items-center justify-between px-4',
     mobileSidebar: 'block md:!hidden',
-    userNav: 'flex items-center gap-9 py-4',
+    userNav: 'flex items-center gap-6 py-4',
   },
 })
 
