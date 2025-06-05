54cc5537d1713cdbe384c35855ef1b86015b5c49
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-06-05 17:41:06 -0600

    pre-commit
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- apps/console/src/app/(auth)/signup/page.tsx
+++ apps/console/src/app/(auth)/signup/page.tsx
@@ -10,13 +10,15 @@ const AuthLogin: React.FC = () => {
       <Suspense>
         <div className="flex h-full w-full min-h-screen gap-4 sm:gap-8 md:gap-12 lg:gap-20 xl:gap-60 justify-center lg:justify-start">
           <AuthMarketingPanel>
-          <h2 className="text-4xl font-normal">
+            <h2 className="text-4xl font-normal">
               Built for Developers,
-              <br/>Loved by Auditors
+              <br />
+              Loved by Auditors
             </h2>
             <p className="text-xl font-muted-foreground">
               Join us at Openlane,
-              <br/>because compliance isn't just a checkbox.
+              <br />
+              because compliance isn't just a checkbox.
             </p>
           </AuthMarketingPanel>{' '}
           <SignupPage />
