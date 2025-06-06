ea738953ff403ca278b9a20ca8e37cb21c231d85
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-06-06 15:15:09 -0600

    feat: rounded-md for checkbox (#556)
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- packages/ui/src/checkbox/checkbox.styles.tsx
+++ packages/ui/src/checkbox/checkbox.styles.tsx
@@ -2,7 +2,7 @@ import { tv, type VariantProps } from 'tailwind-variants'
 
 export const checkboxStyles = tv({
   slots: {
-    root: 'peer h-5 w-5 shrink-0 rounded-sm border border-accent-secondary-muted ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-jade-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:text-teal-900 ',
+    root: 'peer h-5 w-5 shrink-0 rounded-md border border-accent-secondary-muted ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-jade-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:text-teal-900 ',
     indicator: 'flex items-center justify-center text-current',
     checkIcon: 'h-3 w-3 text-text',
   },
