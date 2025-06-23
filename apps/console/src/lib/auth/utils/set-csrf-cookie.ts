65f01525c45f02c193f465fff9f784a0ee7198cd
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-06-22 20:05:17 -0600

    cookie set
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- apps/console/src/lib/auth/utils/set-csrf-cookie.ts
+++ apps/console/src/lib/auth/utils/set-csrf-cookie.ts
@@ -1,10 +1,22 @@
 'use server'
 
-import { csrfCookieName } from '@repo/dally/auth'
+import { csrfCookieName, isDevelopment } from '@repo/dally/auth'
 import { cookies } from 'next/headers'
 
 export const setCSRFCookie = async (csrfToken: string) => {
   const cookieStore = await cookies()
 
-  cookieStore.set(`${csrfCookieName}`, csrfToken)
+  if (isDevelopment) {
+    cookieStore.set(`${csrfCookieName}`, csrfToken, {
+      sameSite: 'lax',
+      secure: false,
+      path: '/',
+    })
+  } else {
+    cookieStore.set(`${csrfCookieName}`, csrfToken, {
+      sameSite: 'none',
+      secure: true,
+      path: '/',
+    })
+  }
 }
