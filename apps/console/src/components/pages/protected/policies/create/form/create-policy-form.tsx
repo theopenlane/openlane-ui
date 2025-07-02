af272d9266b46b28eaa5e33b301220f2c6183751
Mateo Kantoci <info@mateo-it.com>
2025-07-03 00:05:35 +0200

    policy-form-fix: fixed issue with rendering policy form

--- apps/console/src/components/pages/protected/policies/create/form/create-policy-form.tsx
+++ apps/console/src/components/pages/protected/policies/create/form/create-policy-form.tsx
@@ -24,8 +24,6 @@ import { DOCS_URL } from '@/constants'
 import AuthorityCard from '@/components/pages/protected/policies/view/cards/authority-card.tsx'
 import { useOrganization } from '@/hooks/useOrganization'
 import { useGetOrganizationNameById } from '@/lib/graphql-hooks/organization'
-const { currentOrgId } = useOrganization()
-const { data: orgNameData } = useGetOrganizationNameById(currentOrgId)
 
 type TCreatePolicyFormProps = {
   policy?: InternalPolicyByIdFragment
@@ -52,6 +50,8 @@ const CreatePolicyForm: React.FC<TCreatePolicyFormProps> = ({ policy }) => {
   const [metadata, setMetadata] = useState<TMetadata>()
   const isEditable = !!policy
   const [initialAssociations, setInitialAssociations] = useState<TObjectAssociationMap>({})
+  const { currentOrgId } = useOrganization()
+  const { data: orgNameData } = useGetOrganizationNameById(currentOrgId)
 
   const isPoliciesCreate = path === '/policies/create'
 
@@ -252,7 +252,7 @@ const CreatePolicyForm: React.FC<TCreatePolicyFormProps> = ({ policy }) => {
                 <AlertDescription>
                   <p>
                     For template library and help docs, please refer to our{' '}
-                    <a className="text-blue-600" href={`${DOCS_URL}/docs/category/policies-and-procedures`} target="_blank">
+                    <a className="text-blue-600" href={`${DOCS_URL}/docs/category/policies-and-procedures`} target="_blank" rel="noreferrer">
                       documentation
                     </a>
                     .
