65c1ee3535a0c39c27683d2cea191900f4848a98
Bruno Kucevic <brunokucevic@gmail.com>
2025-07-04 20:35:42 +0200

    fix
    Signed-off-by: Bruno Kucevic <brunokucevic@gmail.com>

--- apps/console/src/app/(protected)/layout.tsx
+++ apps/console/src/app/(protected)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { DashboardLayout } from '@/components/layouts/dashboard/dashboard'
 import { auth } from '@/lib/auth/auth'
 import { sessionCookieName } from '@repo/dally/auth'
-import { getDashboardData } from '../api/getDashboardData/route'
+import { getDashboardData, OrganizationsData } from '../api/getDashboardData/route'
 import { cookies } from 'next/headers'
 
 interface OrganizationNode {
@@ -14,16 +14,20 @@ interface OrganizationEdge {
   node: OrganizationNode
 }
 
+let currentOrganizationId: string
+let dashboardData: OrganizationsData | null
 export async function generateMetadata(): Promise<Metadata> {
   const session = await auth()
   const cookieStore = await cookies()
   const cookieSession = cookieStore.get(sessionCookieName as string)
-
   const token = session?.user?.accessToken
   const organizationId = session?.user?.activeOrganizationId
 
   if (cookieSession?.value && token) {
-    const dashboardData = await getDashboardData(token, cookieSession?.value)
+    if (organizationId !== currentOrganizationId) {
+      currentOrganizationId = organizationId
+      dashboardData = await getDashboardData(token, cookieSession?.value)
+    }
     if (dashboardData) {
       const organizations: OrganizationEdge[] = dashboardData.organizations.edges
       const org = organizations.find(({ node }) => node.id === organizationId)
