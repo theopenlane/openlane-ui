6e3cdd43fd44aa15385912d1a121989f4d1d49df
papistacoding <bruno.papista@gmail.com>
2025-07-07 20:41:02 +0200

    custom fields

--- apps/console/src/providers/chatSdk.tsx
+++ apps/console/src/providers/chatSdk.tsx
@@ -4,13 +4,18 @@ import { useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 import { useTheme } from 'next-themes'
 import { chatAppId } from '@repo/dally/auth'
+import { useOrganization } from '@/hooks/useOrganization'
 
 export const InitPlugSDK = () => {
   const { resolvedTheme } = useTheme()
   const { data: session, status } = useSession()
+  const { getOrganizationByID, currentOrgId } = useOrganization()
+
+  const currentOrganization = getOrganizationByID(currentOrgId!)
+  const orgName = currentOrganization?.node?.displayName
 
   useEffect(() => {
-    if (status !== 'authenticated' || !resolvedTheme || !chatAppId || typeof window === 'undefined' || typeof window.plugSDK?.init !== 'function' || window.plugSDK.__plug_initialized__) {
+    if (status !== 'authenticated' || !resolvedTheme || !chatAppId || typeof window === 'undefined' || typeof window.plugSDK?.init !== 'function' || window.plugSDK.__plug_initialized__ || !orgName) {
       return
     }
 
@@ -22,12 +27,12 @@ export const InitPlugSDK = () => {
         user_traits: {
           display_name: session?.user?.name,
           email: session?.user?.email,
-          custom_fields: {},
+          custom_fields: { orgName, orgID: currentOrgId },
         },
       },
     })
     window.plugSDK.__plug_initialized__ = true
-  }, [status, resolvedTheme, session])
+  }, [status, resolvedTheme, session, orgName, currentOrgId])
 
   return null
 }
