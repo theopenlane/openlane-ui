f37e87c29fc865e3b5d033f8d9b3e958c85bfa32
Mateo Kantoci <73197661+mateo-it@users.noreply.github.com>
2025-07-18 16:55:19 +0200

    evidence_fixes: fixed issue with clicking hoverable content that was … (#720)
    * evidence_fixes: fixed issue with clicking hoverable content that was opening sidebar, renamed updated by to last updated by and added as default visible column, removed description from default visible column
    
    * evidence_fixes: implemented status hover actions and filters
    
    * evidence_fixes: implemented file chip on name hover, added URL to form, fixed edit values on evidence edit, ...
    
    * evidence_fixes: fixed imports, and fixed invalidate cache

---
+++ apps/console/src/components/pages/protected/evidence/chart/evidence-status-chip.tsx
@@ -0,0 +1,122 @@
+import React, { useState } from 'react'
+import { Badge } from '@repo/ui/badge'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@repo/ui/tooltip'
+import { useGetFirstFiveEvidencesByStatus } from '@/lib/graphql-hooks/evidence.ts'
+import { CircleQuestionMark, Fingerprint, Folder } from 'lucide-react'
+import Link from 'next/link'
+import { useControlEvidenceStore } from '@/components/pages/protected/controls/hooks/useControlEvidenceStore.ts'
+import { TChardData } from '@/components/pages/protected/evidence/chart/evidence-summary-card.tsx'
+import { ChartColorsSequence } from '@/components/shared/enum-mapper/evidence-enum.tsx'
+
+type TEvidenceStatusChipProps = {
+  data: TChardData
+  programId: string
+  index: number
+}
+
+const EvidenceStatusChip: React.FC<TEvidenceStatusChipProps> = ({ programId, index, data }) => {
+  const [tooltipOpen, setTooltipOpen] = useState(false)
+
+  return (
+    <TooltipProvider delayDuration={300}>
+      <Tooltip open={tooltipOpen} onOpenChange={setTooltipOpen}>
+        <TooltipTrigger asChild>
+          <Badge className="text-white px-2 py-1 text-xs font-normal cursor-pointer" style={{ backgroundColor: ChartColorsSequence[index] }}>
+            {data.name}
+          </Badge>
+        </TooltipTrigger>
+
+        {tooltipOpen && (
+          <TooltipContent side="top" className="max-w-[550px]">
+            <EvidenceTooltipContent evidenceData={data} programId={programId} />
+          </TooltipContent>
+        )}
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
+
+type TEvidenceTooltipContentProps = {
+  evidenceData: TChardData
+  programId: string
+}
+
+const EvidenceTooltipContent: React.FC<TEvidenceTooltipContentProps> = ({ programId, evidenceData }) => {
+  const { data, isLoading } = useGetFirstFiveEvidencesByStatus(evidenceData.status, programId)
+  const { setSelectedControlEvidence } = useControlEvidenceStore()
+
+  if (isLoading) {
+    return <p className="text-xs">Loading details…</p>
+  }
+
+  const filters = [
+    {
+      field: 'status',
+      value: evidenceData.status,
+      type: 'selectIs',
+      operator: 'EQ',
+      label: 'Status',
+    },
+  ]
+
+  const encodedFilters = encodeURIComponent(JSON.stringify(filters))
+  const evidences = data?.evidences?.edges || []
+  const columnClassMap = {
+    1: 'grid-cols-1',
+    2: 'grid-cols-2',
+    3: 'grid-cols-3',
+  } as const
+
+  const columns = Math.min(Math.max(evidences.length, 1), 3) as 1 | 2 | 3
+  const columnClass = columnClassMap[columns]
+
+  return (
+    <div className="bg-background-secondary p-3 rounded-md text-xs">
+      <div className="grid grid-cols-[auto_1fr] gap-y-2">
+        <div className="flex items-center gap-1 border-b pb-2">
+          <CircleQuestionMark size={12} />
+          <span className="font-medium">Description</span>
+        </div>
+        <div className="w-full border-b">
+          <span className="size-fit pl-3 pb-2 flex items-center gap-1">{evidenceData.description}</span>
+        </div>
+
+        <div className="flex items-center gap-1 border-b pb-2">
+          <Folder size={12} />
+          <span className="font-medium">Browse by filter</span>
+        </div>
+        <div className="w-full border-b">
+          <Link href={`/evidence?programId=${programId}&regularFilters=${encodedFilters}&filterActive=1`}>
+            <span className="text-brand size-fit pl-3 pb-2 hover:underline flex items-center gap-1 cursor-pointer">{evidenceData.name}</span>
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-1">
+          <Fingerprint size={12} />
+          <span className="font-medium">Evidences</span>
+        </div>
+
+        {evidences.length === 0 && <span className="pl-3 text-brand text-xs">No evidence available.</span>}
+        {evidences.length > 0 && (
+          <div className={`grid gap-2 pl-3 ${columnClass}`}>
+            {evidences.map((item, index) => (
+              <span key={index} className="pr-1 text-brand text-xs hover:underline cursor-pointer" onClick={() => setSelectedControlEvidence(item?.node?.id ?? null)}>
+                {item?.node?.displayID}
+              </span>
+            ))}
+
+            {evidenceData.value > 5 && (
+              <div className="flex items-center">
+                <div className="flex items-center gap-1 px-3 py-1 rounded-full bg-muted text-muted-foreground text-xs fborder hover:bg-muted/80 transition cursor-pointer">
+                  <span>+{evidenceData.value - 5} more</span>
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default React.memo(EvidenceStatusChip)
---
+++ apps/console/src/components/pages/protected/evidence/chart/evidence-summary-card.tsx
@@ -0,0 +1,62 @@
+'use client'
+import { useGetEvidenceCountsByStatus } from '@/lib/graphql-hooks/evidence.ts'
+import { Card, CardContent } from '@repo/ui/cardpanel'
+import { DonutChart } from '@repo/ui/donut-chart'
+import { useSearchParams } from 'next/navigation'
+import EvidenceStatusChip from '@/components/pages/protected/evidence/chart/evidence-status-chip.tsx'
+import { EvidenceEvidenceStatus } from '@repo/codegen/src/schema.ts'
+import { ChartColorsSequence } from '@/components/shared/enum-mapper/evidence-enum'
+
+export type TChardData = {
+  name: string
+  value: number
+  status: EvidenceEvidenceStatus
+  description: string
+}
+
+export function EvidenceSummaryCard() {
+  const searchParams = useSearchParams()
+  const programId = searchParams.get('programId')
+
+  const { data, isLoading } = useGetEvidenceCountsByStatus(programId)
+
+  const approvedCount = data?.approved?.totalCount ?? 0
+  const rejectedCount = data?.rejected?.totalCount ?? 0
+  const readyCount = data?.ready?.totalCount ?? 0
+  const missingArtifactCount = data?.missingArtifact?.totalCount ?? 0
+  const needsRenewalCount = data?.needsRenewal?.totalCount ?? 0
+
+  const chartData: TChardData[] = [
+    { name: 'Ready', value: readyCount, status: EvidenceEvidenceStatus.READY, description: 'Indicates that the evidence is ready for auditor review.' },
+    { name: 'Approved', value: approvedCount, status: EvidenceEvidenceStatus.APPROVED, description: 'Indicates that the evidence has been approved by the auditor' },
+    { name: 'Needs Renewal', value: needsRenewalCount, status: EvidenceEvidenceStatus.NEEDS_RENEWAL, description: 'Indicates that the evidence needs to be renewed' },
+    { name: 'Missing Artifact', value: missingArtifactCount, status: EvidenceEvidenceStatus.MISSING_ARTIFACT, description: 'Indicates that the evidence is missing an artifact' },
+    { name: 'Rejected', value: rejectedCount, status: EvidenceEvidenceStatus.REJECTED, description: 'Indicates that the evidence has been rejected by the auditor' },
+  ]
+
+  const totalValue = chartData.reduce((acc, item) => acc + item.value, 0)
+
+  const donutChartData = totalValue > 0 ? chartData : [{ name: 'No data', value: 1 }]
+  const donutChartColors = totalValue > 0 ? ChartColorsSequence : ['#E5E7EB']
+
+  return (
+    <Card className="p-6 mb-10">
+      <div className="flex justify-between items-center">
+        <p className="text-lg">Evidence Status Overview</p>
+      </div>
+      <CardContent className="flex items-center gap-8">
+        <div className="w-[100px] h-[100px] shrink-0 mr-8">
+          <DonutChart data={donutChartData} colors={donutChartColors} width={130} height={130} innerRadius={45} outerRadius={65} />
+        </div>
+        <div className="flex gap-8 flex-wrap">
+          {chartData.map((item, i) => (
+            <div key={item.name} className="flex flex-col items-center gap-1">
+              <div className="text-2xl font-semibold">{isLoading ? '...' : item.value}</div>
+              <EvidenceStatusChip data={item} programId={programId!} index={i} />
+            </div>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
