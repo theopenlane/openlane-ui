d7a039aa3b769cb4c12891ebe4ef0a606a48af77
Bruno Papista <bruno.papista@gmail.com>
2025-08-01 23:14:05 +0200

    adding new var

--- packages/dally/src/lib/chat.ts
+++ packages/dally/src/lib/chat.ts
@@ -1,2 +1,3 @@
 export const bedrockModelArn = process.env.BEDROCK_MODEL_ARN
 export const enableChat = process.env.NEXT_PUBLIC_ENABLE_CHATBOT
+export const enableDevrevChat = process.env.NEXT_PUBLIC_ENABLE_DEVREV_CHAT
