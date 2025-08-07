f05d4e88831142a6420076356dd9e00359abe240
papistacoding <bruno.papista@gmail.com>
2025-08-07 16:09:16 +0200

    remove placeholder data on org switch

--- apps/console/src/components/shared/organization-selector/organization-selector.tsx
+++ apps/console/src/components/shared/organization-selector/organization-selector.tsx
@@ -85,6 +85,7 @@ export const OrganizationSelector = () => {
         })
 
         requestAnimationFrame(() => {
+          queryClient?.clear()
           queryClient?.invalidateQueries()
         })
 
