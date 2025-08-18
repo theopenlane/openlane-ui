f829d3d0cf7b172c768b71bdac9935a1fcdd7209
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-08-18 13:49:48 -0600

    get cookies working
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- apps/console/src/app/api/integrations/route.ts
+++ apps/console/src/app/api/integrations/route.ts
@@ -1,5 +1,6 @@
 import { auth } from '@/lib/auth/auth'
 import { secureFetch } from '@/lib/auth/utils/secure-fetch'
+import { isDevelopment } from '@repo/dally/auth'
 import { NextRequest, NextResponse } from 'next/server'
 
 type StartBody = {
@@ -31,11 +32,58 @@ export async function POST(req: NextRequest) {
 
   console.log('RESPONSEEEEEEEEEEEEEEEEEEEE', r)
 
+  // get cookies so they can be set for the call-back
+  const cookieHeader = r.headers.get('set-cookie')
+  const oauthCookies = await getOauthCookies(cookieHeader)
+
+  let response: NextResponse
+
   if (!r.ok) {
     const msg = await r.text()
-    return NextResponse.json({ error: msg || 'Failed to start OAuth' }, { status: r.status })
+    response = NextResponse.json({ error: msg || 'Failed to start OAuth' }, { status: r.status })
+  } else {
+    const json = await r.json()
+    response = NextResponse.json(json)
   }
 
-  const json = await r.json()
-  return NextResponse.json(json)
+  await setOauthCookies(oauthCookies, response)
+
+  return response
+}
+
+export const getOauthCookies = async (cookies: string | null): Promise<Map<string, string>[] | undefined> => {
+  if (!cookies) return undefined
+
+  const cookieArray = cookies.split(', ')
+  const oauthCookies: Map<string, string>[] = []
+  for (const cookie of cookieArray) {
+    if (cookie.startsWith(`oauth_`)) {
+      const key = cookie.split('=')[0]
+      const value = cookie.split(key + '=')[1]
+
+      oauthCookies.push(new Map([[key, value]]))
+    }
+  }
+
+  return oauthCookies.length > 0 ? oauthCookies : undefined
+}
+export const setOauthCookies = async (cookies: Map<string, string>[] | undefined = [], response: NextResponse<unknown>) => {
+  if (!cookies) {
+    return
+  }
+
+  let sameSite = 'Lax'
+  let secure = false
+
+  if (!isDevelopment) {
+    secure = true
+    sameSite = 'None'
+  }
+
+  // Set the cookie using the raw Set-Cookie header to avoid encoding
+  for (const cookie of cookies) {
+    for (const [key, value] of cookie.entries()) {
+      response.headers.append('Set-Cookie', `${key}=${value}; Path=/; SameSite=${sameSite}; Secure=${secure}`)
+    }
+  }
 }
