27f95daa3ccf2805a39a5c49df54bca4bca6b776
papistacoding <bruno.papista@gmail.com>
2025-08-28 16:21:15 +0200

    fixing roles

--- apps/console/src/components/pages/protected/controls/control-implementations-section.tsx
+++ apps/console/src/components/pages/protected/controls/control-implementations-section.tsx
@@ -1,23 +1,31 @@
 import { CreateButton } from '@/components/shared/create-button/create-button'
 import usePlateEditor from '@/components/shared/plate/usePlateEditor'
+import { useOrganizationRole } from '@/lib/authz/access-api'
+import { AccessEnum } from '@/lib/authz/enums/access-enum'
+import { canCreate } from '@/lib/authz/utils'
 import { GetControlByIdQuery } from '@repo/codegen/src/schema'
 import { Button } from '@repo/ui/button'
 import { Label } from '@repo/ui/label'
 import { ChevronRight } from 'lucide-react'
+import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { useParams } from 'next/navigation'
 import React from 'react'
 
 type Props = {
   controlImplementations: GetControlByIdQuery['control']['controlImplementations']
-  canEdit: boolean
 }
 
-const ControlImplementationsSection = ({ controlImplementations, canEdit }: Props) => {
+const ControlImplementationsSection = ({ controlImplementations }: Props) => {
   const { convertToReadOnly } = usePlateEditor()
   const params = useParams()
   const id = params?.id as string
   const subcontrolId = params?.subcontrolId as string | undefined
+
+  const { data: session } = useSession()
+  const { data: orgPermission } = useOrganizationRole(session)
+
+  const createAllowed = canCreate(orgPermission?.roles, AccessEnum.CanCreateControlObjective)
 
   const edges = controlImplementations?.edges?.filter((edge): edge is NonNullable<typeof edge> & { node: { details: string } } => !!edge?.node?.details) ?? []
 
@@ -30,7 +38,7 @@ const ControlImplementationsSection = ({ controlImplementations, canEdit }: Prop
     <div className="flex flex-col gap-2">
       <div className="flex gap-2 items-center">
         <Label className="font-semibold text-lg">{edges.length === 1 ? 'Control Implementation' : 'Control Implementations'}</Label>
-        {canEdit && <CreateButton type="control-implementation" href={createHref} ariaLabel="Create control implementation" />}
+        {createAllowed && <CreateButton type="control-implementation" href={createHref} ariaLabel="Create control implementation" />}
         <Link href={controlImplementationPath}>
           <Button type="button" className="!h-8 !p-2 size-fit" variant="outline" icon={<ChevronRight size={16} />}>
             View
