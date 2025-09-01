a2f9f971409499442ae8ef96d5064422f6e4a936
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-09-01 15:23:02 -0600

    fix bad suggestion
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- apps/console/src/components/shared/plate/plate-editor.tsx
+++ apps/console/src/components/shared/plate/plate-editor.tsx
@@ -41,6 +41,7 @@ const PlateEditor: React.FC<TPlateEditorProps> = ({ onChange, initialValue, vari
       let slateNodes
 
       switch (fmt) {
+        case 'markdown':
           slateNodes = (plateEditor.api.markdown?.deserialize?.(initialValue || '') ?? []) as Value
           break
         default:
