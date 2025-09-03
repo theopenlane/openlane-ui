11373223e9d5e89b1ff706128e085fe6964b6719
papistacoding <bruno.papista@gmail.com>
2025-09-03 15:11:32 +0200

    llinter

--- apps/console/src/app/(protected)/controls/[id]/page.tsx
+++ apps/console/src/app/(protected)/controls/[id]/page.tsx
@@ -305,17 +305,16 @@ const ControlDetailsPage: React.FC = () => {
               </>
             }
           />
-          {canEdit(permission?.roles) ||
-            (canDelete(permission?.roles) && (
-              <div className="flex gap-2">
-                {canEdit(permission?.roles) && (
-                  <Button type="button" variant="outline" className="!p-1 h-8 bg-card" onClick={(e) => handleEdit(e)} aria-label="Edit control">
-                    <PencilIcon size={16} strokeWidth={2} />
-                  </Button>
-                )}
-                {canDelete(permission?.roles) && <DeleteControlDialog controlId={control.id} refCode={control.refCode} />}
-              </div>
-            ))}
+          {(canEdit(permission?.roles) || canDelete(permission?.roles)) && (
+            <div className="flex gap-2">
+              {canEdit(permission?.roles) && (
+                <Button type="button" variant="outline" className="!p-1 h-8 bg-card" onClick={(e) => handleEdit(e)} aria-label="Edit control">
+                  <PencilIcon size={16} strokeWidth={2} />
+                </Button>
+              )}
+              {canDelete(permission?.roles) && <DeleteControlDialog controlId={control.id} refCode={control.refCode} />}
+            </div>
+          )}
         </div>
       )}
     </div>
