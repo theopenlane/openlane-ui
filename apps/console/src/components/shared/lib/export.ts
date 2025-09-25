70542743b8f0294d59e85a37b870a28a378fd126
Bruno Kucevic <brunokucevic@gmail.com>
2025-09-25 09:08:31 +0200

    download fixed
    Signed-off-by: Bruno Kucevic <brunokucevic@gmail.com>

--- apps/console/src/components/shared/lib/export.ts
+++ apps/console/src/components/shared/lib/export.ts
@@ -1,11 +1,19 @@
 export const fileDownload = async (presignedURL: string, fileName: string, errorNotification: (args: { title: string; variant: 'destructive' | 'default' }) => void) => {
   try {
+    const response = await fetch(presignedURL)
+    if (!response.ok) throw new Error('Failed to fetch file')
+
+    const blob = await response.blob()
+    const url = window.URL.createObjectURL(blob)
+
     const link = document.createElement('a')
-    link.href = presignedURL
+    link.href = url
     link.setAttribute('download', fileName)
     document.body.appendChild(link)
     link.click()
     document.body.removeChild(link)
+
+    window.URL.revokeObjectURL(url)
   } catch {
     errorNotification({
       title: 'An error occurred while downloading file. Please try again.',
