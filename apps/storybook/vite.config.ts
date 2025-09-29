2274c6cc5c7f1be591731a271bb6a899c5046a6e
Mateo Kantoci <info@mateo-it.com>
2025-09-29 23:17:42 +0200

    ISS-411: fixing build - storybook

--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
