aaf2fcf0253a9ea657dffb08de5edb5af89d01de
Mateo Kantoci <info@mateo-it.com>
2025-09-29 14:39:46 +0200

    ISS-411: fixed switch style

--- packages/ui/src/switch/switch.styles.tsx
+++ packages/ui/src/switch/switch.styles.tsx
@@ -2,8 +2,9 @@ import { tv, type VariantProps } from 'tailwind-variants'
 
 export const switchStyles = tv({
   slots: {
-    base: 'peer bg-button-muted inline-flex h-4 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=unchecked]:bg-gray-300',
-    thumb: 'pointer-events-none block h-4 w-4 rounded-full bg-white ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0',
+    base: 'peer bg-primary-muted inline-flex h-4 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=unchecked]:bg-primary-muted',
+    thumb:
+      'pointer-events-none block h-4 w-4 rounded-full bg-white ring-0 transition-transform data-[state=checked]:bg-primary data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0',
   },
 })
 
