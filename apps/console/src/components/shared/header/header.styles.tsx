91ec2b0efae338535f7ee7d995e8085c0c1f9ce2
Bruno Papista <78353799+papistacoding@users.noreply.github.com>
2025-10-13 20:45:20 +0200

    sidebars rework (#1010)
    * sidebars rework
    
    * sidebar redesign
    
    * fixing hidden
    
    * store session
    
    * default to expanded
    
    * remove comment
    
    * container fix
    
    * fix logo

--- apps/console/src/components/shared/header/header.styles.tsx
+++ apps/console/src/components/shared/header/header.styles.tsx
@@ -2,7 +2,7 @@ import { tv, type VariantProps } from 'tailwind-variants'
 
 const headerStyles = tv({
   slots: {
-    header: 'sticky left-0 right-0 top-0 z-20 shrink-0 my-2 mb-1',
+    header: 'sticky left-0 right-0 top-0 z-20 shrink-0 my-1',
     nav: 'flex justify-between items-center w-full h-14 py-5 px-6 bg-secondary rounded-xl',
     mobileSidebar: 'block md:hidden',
     userNav: 'flex items-center gap-6',
