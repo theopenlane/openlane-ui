6d005e6bdca27c237d37f7a08d5437511e272d51
Bruno Kucevic <brunokucevic@gmail.com>
2025-10-16 10:33:04 +0200

    removed commented code
    Signed-off-by: Bruno Kucevic <brunokucevic@gmail.com>

--- apps/console/src/components/pages/protected/risks/table/table-config.ts
+++ apps/console/src/components/pages/protected/risks/table/table-config.ts
@@ -53,38 +53,6 @@ export const getRisksFilterFields = (programOptions: { value: string; label: str
   },
 ]
 
-// export const RISKS_FILTER_FIELDS: FilterField[] = [
-//   { key: 'category', label: 'Category', type: 'text', icon: FileQuestion },
-//   {
-//     key: 'impact',
-//     label: 'Impact',
-//     type: 'select',
-//     options: enumToOptions(RiskRiskImpact),
-//     icon: Briefcase,
-//   },
-//   {
-//     key: 'likelihood',
-//     label: 'Likelihood',
-//     type: 'select',
-//     options: enumToOptions(RiskRiskLikelihood),
-//     icon: Briefcase,
-//   },
-//   {
-//     key: 'riskType',
-//     label: 'Risk Type',
-//     type: 'text',
-//     icon: SquareAsterisk,
-//   },
-//   { key: 'score', label: 'Score', type: 'text', icon: Proportions },
-//   {
-//     key: 'status',
-//     label: 'Status',
-//     type: 'select',
-//     options: enumToOptions(RiskRiskStatus),
-//     icon: Tags,
-//   },
-// ]
-
 export const RISKS_SORT_FIELDS = [
   {
     key: RiskOrderField.name,
