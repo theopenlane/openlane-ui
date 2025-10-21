0b8ec7c51cdba54677469555d5564e600455561b
Mateo Kantoci <info@mateo-it.com>
2025-10-21 18:20:31 +0200

    ISS-668: added skeleton for testimonials

--- apps/console/src/components/shared/AuthMarketingPanel.tsx/auth-marketing-panel.tsx
+++ apps/console/src/components/shared/AuthMarketingPanel.tsx/auth-marketing-panel.tsx
@@ -1,13 +1,25 @@
 'use client'
 import LoginBackground from '@/assets/LoginBackground.tsx'
+import Image from 'next/image'
 
 const AuthMarketingPanel = () => {
   return (
     <div className="hidden lg:flex flex-col justify-center rounded-lg w-2/5 w-[560px] relative overflow-hidden">
       <div className="flex flex-col space-y-10 z-10 pl-[64px] pr-[64px]">
-        “Openlane makes compliance effortless. What used to take us weeks of manual work now happens automatically—and the dashboards give our team total visibility.”
+        <span>“Openlane makes compliance effortless. What used to take us weeks of manual work now happens automatically—and the dashboards give our team total visibility.”</span>
+        <div className="flex items-center gap-4 mt-2">
+          <div className="w-12 h-12 rounded-full overflow-hidden border border-muted">
+            <Image src="/" alt="Emily Carter" width={48} height={48} className="object-cover" />
+          </div>
+
+          <div className="flex flex-col">
+            <span className="font-medium text-sm text-foreground">Emily Carter</span>
+            <span className="text-sm text-muted-foreground font-normal">Head of Security, Arctura Labs</span>
+          </div>
+        </div>
       </div>
-      <LoginBackground className="absolute right-0 top-[645px] bottom-0 flex-shrink-0 opacity-50 " />
+
+      <LoginBackground className="absolute right-0 top-[645px] bottom-0 flex-shrink-0 opacity-50" />
     </div>
   )
 }
