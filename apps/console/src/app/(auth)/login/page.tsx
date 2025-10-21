dfc91da9a217d833b5148340ace4a656f24c3262
Mateo Kantoci <info@mateo-it.com>
2025-10-21 16:13:34 +0200

    ISS-668: added margin on left screen on login and auth

--- apps/console/src/app/(auth)/login/page.tsx
+++ apps/console/src/app/(auth)/login/page.tsx
@@ -12,12 +12,12 @@ const AuthLogin: React.FC = () => {
   return (
     <Suspense>
       <div className="flex h-full w-full min-h-screen">
-        <div className="flex flex-col justify-between items-center w-full lg:w-4/5 p-6 bg-secondary relative">
+        <div className="flex flex-col justify-between items-center w-full lg:w-4/5 relative">
           <div className="absolute top-8 left-1/2 transform -translate-x-1/2">
             <Logo width={200} />
           </div>
 
-          <div className="flex justify-center items-center w-full h-full">
+          <div className="flex justify-center items-center w-full h-full mt-3 mb-3 ml-8 z-[999] rounded-lg bg-secondary">
             <LoginPage />
           </div>
         </div>
--- apps/console/src/components/shared/AuthMarketingPanel.tsx/auth-marketing-panel.tsx
+++ apps/console/src/components/shared/AuthMarketingPanel.tsx/auth-marketing-panel.tsx
@@ -3,8 +3,8 @@ import LoginBackground from '@/assets/LoginBackground.tsx'
 
 const AuthMarketingPanel = () => {
   return (
-    <div className="hidden lg:flex flex-col justify-center rounded-lg w-2/5 max-w-[555px] relative overflow-hidden">
-      <div className="flex flex-col space-y-10 ml-5 z-10 pl-[64px] pr-[64px]">
+    <div className="hidden lg:flex flex-col justify-center rounded-lg w-2/5 w-[560px] relative overflow-hidden">
+      <div className="flex flex-col space-y-10 z-10 pl-[64px] pr-[64px]">
         “Openlane makes compliance effortless. What used to take us weeks of manual work now happens automatically—and the dashboards give our team total visibility.”
       </div>
       <LoginBackground className="absolute right-0 top-[645px] bottom-0 flex-shrink-0 opacity-50 " />
