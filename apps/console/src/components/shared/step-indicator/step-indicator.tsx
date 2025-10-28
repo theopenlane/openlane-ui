dc32b6ce4607a0e4f6dd46fae1c928b3ee1d899e
Mateo Kantoci <info@mateo-it.com>
2025-10-28 18:40:18 +0100

    ISS-718: fixing colors on stepper

--- apps/console/src/components/shared/step-indicator/step-indicator.tsx
+++ apps/console/src/components/shared/step-indicator/step-indicator.tsx
@@ -7,8 +7,8 @@ interface StepIndicatorProps {
 const StepIndicator: React.FC<StepIndicatorProps> = ({ active = false }) => {
   return (
     <div className="w-5 h-5 flex items-center justify-center">
-      <div className={active ? 'flex justify-center items-center w-5 h-5 rounded-full border border-btn-secondary bg-step-active-bg' : 'w-2.5 h-2.5 rounded-full bg-indicator'}>
-        {active && <div className="w-2.5 h-2.5 rounded-full bg-btn-secondary" />}
+      <div className={active ? 'flex justify-center items-center w-5 h-5 rounded-full border border-btn-primary bg-step-active-bg' : 'w-2.5 h-2.5 rounded-full bg-indicator'}>
+        {active && <div className="w-2.5 h-2.5 rounded-full bg-btn-primary" />}
       </div>
     </div>
   )
