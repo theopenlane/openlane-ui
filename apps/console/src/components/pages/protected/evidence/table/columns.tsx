478949e292d76cc59ed58032c27a98734d04059a
adripo <26493496+adripo@users.noreply.github.com>
2025-10-30 23:48:39 +0100

    fix: Update field key
    Co-authored-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    Signed-off-by: adripo <26493496+adripo@users.noreply.github.com>

--- apps/console/src/components/pages/protected/evidence/table/columns.tsx
+++ apps/console/src/components/pages/protected/evidence/table/columns.tsx
@@ -16,10 +16,10 @@ type TGetEvidenceColumnsProps = {
 export const getEvidenceColumns = ({ userMap }: TGetEvidenceColumnsProps) => {
   const columns: ColumnDef<Evidence>[] = [
     {
-      accessorKey: 'displayID',
+      accessorKey: 'id',
       header: 'ID',
       size: 120,
-      cell: ({ row }) => <div className="text-muted-foreground">{row.original.displayID || row.original.id}</div>,
+      cell: ({ row }) => <div className="text-muted-foreground">{row.original.id}</div>,
     },
     {
       accessorKey: 'name',
