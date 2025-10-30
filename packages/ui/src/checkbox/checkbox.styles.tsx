f07bc6f3473ba26f3bc149af0d0da2c58a698323
Mateo Kantoci <73197661+mateo-it@users.noreply.github.com>
2025-10-30 20:48:05 +0100

    ISS-764: fixed checkbox to be same style on filter, and fixed sidebar panel to have margin like main content panel (#1092)

--- packages/ui/src/checkbox/checkbox.styles.tsx
+++ packages/ui/src/checkbox/checkbox.styles.tsx
@@ -2,13 +2,9 @@ import { tv, type VariantProps } from 'tailwind-variants'
 
 export const checkboxStyles = tv({
   slots: {
-    root:
-      'peer h-5 w-5 shrink-0 bg-btn-secondary rounded-md border border-border ring-offset-white ' +
-      'focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-jade-950 ' +
-      'focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 ' +
-      'data-[state=checked]:text-teal-900 data-[state=checked]:bg-teal-500',
+    root: 'peer h-5 w-5 shrink-0 bg-btn-secondary rounded-md border border-border ring-offset-white text-primary',
     indicator: 'flex items-center justify-center text-current',
-    checkIcon: 'h-3 w-3 text-brand-900',
+    checkIcon: 'h-4 w-4 text-brand-900',
   },
 })
 
