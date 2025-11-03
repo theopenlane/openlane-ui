0d8bdeb8d8f2377963f9a5251465f0c7d87d029e
bruno papista <bruno.papista@gmail.com>
2025-11-03 19:29:39 +0100

    fixing how renewing/canceling works

--- apps/console/src/components/pages/protected/organization/billing/billing-settings.tsx
+++ apps/console/src/components/pages/protected/organization/billing/billing-settings.tsx
@@ -34,6 +34,7 @@ const BillingSettings: React.FC = () => {
   const schedule = schedules?.[0]
   const isCanceledBySchedule = schedule?.end_behavior === 'cancel'
   const { data: paymentData } = usePaymentMethodsQuery(stripeCustomerId)
+  const isTrialing = !!schedule?.phases?.[0]?.trial
 
   const defaultCard = useMemo(() => {
     return paymentData?.defaultPaymentMethod?.card ? paymentData.defaultPaymentMethod.card : paymentData?.paymentMethods?.[0]?.card
@@ -143,23 +144,34 @@ const BillingSettings: React.FC = () => {
       <Invoices stripeCustomerId={stripeCustomerId} />
 
       {/* Cancel Section */}
-      <h2 className="text-2xl mt-8 mb-4">Cancel Subscription</h2>
+      {isTrialing && isCanceledBySchedule ? (
+        <>
+          <p className="mt-8"> Your trial subscription was cancelled and cannot be renewed.</p>
+          <a href={SUPPORT_URL} target="_blank" rel="noopener noreferrer" className="mt-2 mx-auto mb-5 block">
+            <Button>Contact Support</Button>
+          </a>
+        </>
+      ) : (
+        <>
+          <h2 className="text-2xl mt-8 mb-4">Cancel Subscription</h2>
 
-      {data?.organization?.orgSubscriptions && data?.organization?.orgSubscriptions.length > 0 && (
-        <div id="cancel-subscription">
-          <div className="flex flex-col md:flex-row md:items-start justify-between gap-3 w-full">
-            <p className={cn(text())}>You can cancel your subscription anytime. Your access will remain active until the end of your billing period.</p>
-          </div>
-          {schedule ? (
-            <Button className="self-end h-8 p-2" variant={isCanceledBySchedule ? 'secondary' : 'destructive'} disabled={canceling || schedulesLoading} onClick={() => setConfirmCancelOpen(true)}>
-              {canceling ? 'Processing…' : isCanceledBySchedule ? 'Renew subscription' : 'Cancel subscription'}
-            </Button>
-          ) : (
-            <a href={`${SUPPORT_URL}`} target="_blank" rel="noopener noreferrer" className="text-sm text-blue-500">
-              Reach out to support
-            </a>
+          {data?.organization?.orgSubscriptions && data?.organization?.orgSubscriptions.length > 0 && (
+            <div id="cancel-subscription">
+              <div className="flex flex-col md:flex-row md:items-start justify-between gap-3 w-full">
+                <p className={cn(text())}>You can cancel your subscription anytime. Your access will remain active until the end of your billing period.</p>
+              </div>
+              {schedule ? (
+                <Button className="self-end h-8 p-2" variant={isCanceledBySchedule ? 'secondary' : 'destructive'} disabled={canceling || schedulesLoading} onClick={() => setConfirmCancelOpen(true)}>
+                  {canceling ? 'Processing…' : isCanceledBySchedule ? 'Renew subscription' : 'Cancel subscription'}
+                </Button>
+              ) : (
+                <a href={`${SUPPORT_URL}`} target="_blank" rel="noopener noreferrer" className="text-sm text-blue-500">
+                  Reach out to support
+                </a>
+              )}
+            </div>
           )}
-        </div>
+        </>
       )}
 
       {/* Cancel subscription confirmation */}
