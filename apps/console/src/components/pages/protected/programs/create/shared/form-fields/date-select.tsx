f979c267b09f47a609f2b799c28fd295086b15c1
bruno papista <bruno.papista@gmail.com>
2025-11-06 17:22:10 +0100

    Program setup minor styling bugs, calendar redesign and customSelect remove

--- apps/console/src/components/pages/protected/programs/create/shared/form-fields/date-select.tsx
+++ apps/console/src/components/pages/protected/programs/create/shared/form-fields/date-select.tsx
@@ -12,16 +12,7 @@ export const DateSelect = ({ name }: { name: string }) => {
       render={({ field }) => (
         <FormItem className={'flex flex-col space-y-0'}>
           <FormControl>
-            <CalendarPopover
-              field={field}
-              defaultToday
-              customSelect={[
-                { value: 0, label: 'Today' },
-                { value: 1, label: 'Tomorrow' },
-                { value: 7, label: 'In 1 week' },
-                { value: 30, label: 'In 1 month' },
-              ]}
-            />
+            <CalendarPopover field={field} defaultToday />
           </FormControl>
         </FormItem>
       )}
