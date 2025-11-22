95e6a671b9b2e51c3782be8fd2ad1d325d3eaee3
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-11-22 16:58:31 -0700

    Revert "billing exipred middleware and sidebar changes (#1185)"
    This reverts commit 9e48bd1c9b79ae1aa427930558b63b9737d5be23.

--- apps/console/src/routes/dashboard.tsx
+++ apps/console/src/routes/dashboard.tsx
@@ -32,196 +32,187 @@ import { canEdit } from '@/lib/authz/utils'
 import { TData } from '@/types/authz'
 import type { Session } from 'next-auth'
 
-export const topNavigationItems = (session: Session | null): (NavItem | Separator | NavHeading)[] => {
-  const billingExpired = session?.user.modules.length === 0
-  return [
-    {
-      type: 'separator',
-    },
-    {
-      title: 'Home',
-      href: '/dashboard',
-      icon: House,
-      hidden: session?.user?.isOnboarding || billingExpired,
-    },
-    {
-      title: 'Tasks',
-      href: '/tasks',
-      icon: ListChecks,
-      hidden: session?.user?.isOnboarding || billingExpired,
-    },
-    {
-      title: 'Compliance',
-      plan: PlanEnum.COMPLIANCE_MODULE,
-      icon: ShieldCheck,
-      href: '/',
-      hidden: session?.user?.isOnboarding || billingExpired,
-      children: [
-        {
-          title: 'Programs',
-          href: '/programs',
-          icon: ShieldCheck,
-        },
-        {
-          title: 'Controls',
-          href: '/controls',
-          params: '?tab=report',
-          icon: Settings2,
-        },
-        {
-          title: 'Evidence',
-          href: '/evidence',
-          icon: Fingerprint,
-        },
-        {
-          title: 'Policies',
-          href: '/policies',
-          icon: ScrollText,
-        },
-        {
-          title: 'Procedures',
-          href: '/procedures',
-          icon: Workflow,
-        },
-        {
-          title: 'Standards Catalog',
-          href: '/standards',
-          icon: FileBadge2,
-        },
-        {
-          title: 'Questionnaires',
-          href: '/questionnaires',
-          icon: NotebookPen,
-        },
-        {
-          title: 'Risks',
-          href: '/risks',
-          icon: AlertTriangle,
-        },
-      ],
-    },
-    {
-      title: 'Trust center',
-      plan: PlanEnum.TRUST_CENTER_MODULE,
-      href: '/trust-center',
-      icon: Handshake,
-      isChildren: true,
-      hidden: session?.user?.isOnboarding || billingExpired,
-      children: [
-        {
-          title: 'Settings',
-          href: '/trust-center/settings',
-          icon: Settings,
-        },
-        {
-          title: 'Reports & Certifications',
-          href: '/trust-center/reports-and-certifications',
-          icon: File,
-        },
-      ],
-    },
-  ]
-}
+export const topNavigationItems = (session?: Session): (NavItem | Separator | NavHeading)[] => [
+  {
+    type: 'separator',
+  },
+  {
+    title: 'Home',
+    href: '/dashboard',
+    icon: House,
+    hidden: session?.user?.isOnboarding,
+  },
+  {
+    title: 'Tasks',
+    href: '/tasks',
+    icon: ListChecks,
+    hidden: session?.user?.isOnboarding,
+  },
+  {
+    title: 'Compliance',
+    plan: PlanEnum.COMPLIANCE_MODULE,
+    icon: ShieldCheck,
+    href: '/',
+    hidden: session?.user?.isOnboarding,
+    children: [
+      {
+        title: 'Programs',
+        href: '/programs',
+        icon: ShieldCheck,
+      },
+      {
+        title: 'Controls',
+        href: '/controls',
+        params: '?tab=report',
+        icon: Settings2,
+      },
+      {
+        title: 'Evidence',
+        href: '/evidence',
+        icon: Fingerprint,
+      },
+      {
+        title: 'Policies',
+        href: '/policies',
+        icon: ScrollText,
+      },
+      {
+        title: 'Procedures',
+        href: '/procedures',
+        icon: Workflow,
+      },
+      {
+        title: 'Standards Catalog',
+        href: '/standards',
+        icon: FileBadge2,
+      },
+      {
+        title: 'Questionnaires',
+        href: '/questionnaires',
+        icon: NotebookPen,
+      },
+      {
+        title: 'Risks',
+        href: '/risks',
+        icon: AlertTriangle,
+      },
+    ],
+  },
+  {
+    title: 'Trust center',
+    plan: PlanEnum.TRUST_CENTER_MODULE,
+    href: '/trust-center',
+    icon: Handshake,
+    isChildren: true,
+    hidden: session?.user?.isOnboarding,
+    children: [
+      {
+        title: 'Settings',
+        href: '/trust-center/settings',
+        icon: Settings,
+      },
+      {
+        title: 'Reports & Certifications',
+        href: '/trust-center/reports-and-certifications',
+        icon: File,
+      },
+    ],
+  },
+]
 
-export const bottomNavigationItems = (orgPermission: TData | undefined, session: Session | null): (NavItem | Separator | NavHeading)[] => {
-  const billingExpired = session?.user.modules.length === 0
-
-  return [
-    {
-      title: 'Organization settings',
-      href: '/organization-settings',
-      hidden: session?.user?.isOnboarding,
-      icon: Building2,
-      children: [
-        {
-          title: 'General Settings',
-          href: '/organization-settings/general-settings',
-          hidden: !canEdit(orgPermission?.roles),
-          icon: SettingsIcon,
-        },
-        {
-          title: 'Authentication',
-          href: '/organization-settings/authentication',
-          icon: GlobeLock,
-          hidden: billingExpired,
-        },
-        {
-          title: 'Subscribers',
-          href: '/organization-settings/subscribers',
-          icon: MailCheck,
-          hidden: true,
-        },
-        {
-          title: 'Billing',
-          href: '/organization-settings/billing',
-          hidden: !canEdit(orgPermission?.roles),
-          icon: DollarSign,
-        },
-        {
-          title: 'Audit Logs',
-          href: '/organization-settings/logs',
-          icon: History,
-          hidden: billingExpired,
-        },
-        {
-          title: 'Integrations',
-          href: '/organization-settings/integrations',
-          // hidden: !canEdit,
-          hidden: true,
-          icon: Workflow,
-        },
-      ],
-    },
-    {
-      title: 'User Management',
-      href: '/user-management',
-      icon: UserRoundPen,
-      hidden: session?.user?.isOnboarding || billingExpired,
-      children: [
-        {
-          title: 'Members',
-          href: '/user-management/members',
-          icon: UserRoundPlus,
-        },
-        {
-          title: 'Groups',
-          href: '/user-management/groups',
-          icon: Users,
-        },
-      ],
-    },
-    {
-      title: 'Developers',
-      href: '/developers',
-      icon: Bot,
-      hidden: session?.user?.isOnboarding || billingExpired,
-      children: [
-        {
-          title: 'API Tokens',
-          href: '/developers/api-tokens',
-          icon: ServerCog,
-        },
-        {
-          title: 'Personal Access Tokens',
-          href: '/developers/personal-access-tokens',
-          icon: KeyRoundIcon,
-        },
-      ],
-    },
-    {
-      title: 'User settings',
-      href: '/user-settings',
-      children: [
-        {
-          title: 'Profile',
-          href: '/user-settings/profile',
-          icon: UserRoundPen,
-        },
-      ],
-      icon: UserCog,
-    },
-  ]
-}
+export const bottomNavigationItems = (orgPermission?: TData, session?: Session): (NavItem | Separator | NavHeading)[] => [
+  {
+    title: 'Organization settings',
+    href: '/organization-settings',
+    hidden: session?.user?.isOnboarding,
+    icon: Building2,
+    children: [
+      {
+        title: 'General Settings',
+        href: '/organization-settings/general-settings',
+        hidden: !canEdit(orgPermission?.roles),
+        icon: SettingsIcon,
+      },
+      {
+        title: 'Authentication',
+        href: '/organization-settings/authentication',
+        icon: GlobeLock,
+      },
+      {
+        title: 'Subscribers',
+        href: '/organization-settings/subscribers',
+        icon: MailCheck,
+        hidden: true,
+      },
+      {
+        title: 'Billing',
+        href: '/organization-settings/billing',
+        hidden: !canEdit(orgPermission?.roles),
+        icon: DollarSign,
+      },
+      {
+        title: 'Audit Logs',
+        href: '/organization-settings/logs',
+        icon: History,
+      },
+      {
+        title: 'Integrations',
+        href: '/organization-settings/integrations',
+        // hidden: !canEdit,
+        hidden: true,
+        icon: Workflow,
+      },
+    ],
+  },
+  {
+    title: 'User Management',
+    href: '/user-management',
+    icon: UserRoundPen,
+    hidden: session?.user?.isOnboarding,
+    children: [
+      {
+        title: 'Members',
+        href: '/user-management/members',
+        icon: UserRoundPlus,
+      },
+      {
+        title: 'Groups',
+        href: '/user-management/groups',
+        icon: Users,
+      },
+    ],
+  },
+  {
+    title: 'Developers',
+    href: '/developers',
+    icon: Bot,
+    hidden: session?.user?.isOnboarding,
+    children: [
+      {
+        title: 'API Tokens',
+        href: '/developers/api-tokens',
+        icon: ServerCog,
+      },
+      {
+        title: 'Personal Access Tokens',
+        href: '/developers/personal-access-tokens',
+        icon: KeyRoundIcon,
+      },
+    ],
+  },
+  {
+    title: 'User settings',
+    href: '/user-settings',
+    children: [
+      {
+        title: 'Profile',
+        href: '/user-settings/profile',
+        icon: UserRoundPen,
+      },
+    ],
+    icon: UserCog,
+  },
+]
 
 export const personalNavigationItems = (): (NavItem | Separator | NavHeading)[] => [
   {
