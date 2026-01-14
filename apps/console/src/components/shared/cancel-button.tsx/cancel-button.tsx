35db55ab5592a48b3e4811d3993eab80b295b803
Bruno Kucevic <brunokucevic@gmail.com>
2026-01-14 16:39:23 +0100

    ISS-915: cancel button

---
+++ apps/console/src/components/shared/cancel-button.tsx/cancel-button.tsx
@@ -0,0 +1,37 @@
+import { Button } from '@repo/ui/button'
+import { SaveIcon } from 'lucide-react'
+type CancelButtonProps = {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
+  title?: string
+  type?: 'button' | 'submit' | 'reset'
+  disabled?: boolean
+  className?: string
+  variant?:
+    | 'primary'
+    | 'secondary'
+    | 'secondaryOutline'
+    | 'icon'
+    | 'iconButton'
+    | 'sidebar'
+    | 'transparent'
+    | 'tag'
+    | 'filled'
+    | 'light'
+    | 'outline'
+    | 'outlineLight'
+    | 'outlineInput'
+    | 'outlineInputPadding'
+    | 'redOutline'
+    | 'white'
+    | 'success'
+    | 'destructive'
+    | 'back'
+}
+
+export const CancelButton = ({ onClick, title = 'Cancel', type = 'button', disabled, className, variant = 'secondary' }: CancelButtonProps) => {
+  return (
+    <Button disabled={disabled} onClick={onClick} variant={variant} type={type} iconPosition="left" className={`h-8 px-2 ${className}`} icon={<SaveIcon />}>
+      {title}
+    </Button>
+  )
+}
