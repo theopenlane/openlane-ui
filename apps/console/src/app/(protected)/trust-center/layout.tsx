dbaaaf9508b7c5d9c348ec19616664f357843dba
BrunoKucevic <42520577+BrunoKucevic@users.noreply.github.com>
2026-01-14 23:43:06 +0100

    fix: add message if no trust center (#1307)
    * fix: add message if no trust center
    
    * fix: added protected area instead of message
    
    * added trust center check after feature gate
    
    * reused error page
    
    * removed error check

--- apps/console/src/app/(protected)/trust-center/layout.tsx
+++ apps/console/src/app/(protected)/trust-center/layout.tsx
@@ -1,11 +1,12 @@
 import { FeatureEnum } from '@/lib/subscription-plan/feature-enum'
 import { PlanEnum } from '@/lib/subscription-plan/plan-enum'
 import { FeatureGate } from '@/lib/subscription-plan/feature-gate'
+import TrustCenter from '@/components/pages/protected/trust-center/trust-center'
 
 export default function EntitiesLayout({ children }: { children: React.ReactNode }) {
   return (
     <FeatureGate feature={FeatureEnum.TRUST_CENTER} module={PlanEnum.TRUST_CENTER_MODULE}>
-      {children}
+      <TrustCenter>{children}</TrustCenter>
     </FeatureGate>
   )
 }
