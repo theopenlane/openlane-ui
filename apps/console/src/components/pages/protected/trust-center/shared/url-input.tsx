bfc4842ec7f220f5cb33314e9bb890515b272c14
Bruno Kucevic <brunokucevic@gmail.com>
2026-01-20 19:56:40 +0100

    test

--- apps/console/src/components/pages/protected/trust-center/shared/url-input.tsx
+++ apps/console/src/components/pages/protected/trust-center/shared/url-input.tsx
@@ -17,7 +17,7 @@ type UrlInputProps = {
 
 function UrlInput({ value, onChange, disabled, className, verifiedStatus, hasCopyButton, placeholder }: UrlInputProps) {
   const isVerified = verifiedStatus === DnsVerificationDnsVerificationStatus.ACTIVE
-  console.log('url input value with next link', value)
+  console.log('url input value', value)
   return (
     <div className={clsx('flex items-center border rounded-md w-[490px]', className)}>
       <p className="px-3 py-2 text-sm select-none">https://</p>
