84c998a96468be66615c39f27c33ee79a906ef98
Bruno Kucevic <brunokucevic@gmail.com>
2026-01-20 18:29:42 +0100

    domain fixes

--- apps/console/src/components/pages/protected/trust-center/branding/sections/branding-header.tsx
+++ apps/console/src/components/pages/protected/trust-center/branding/sections/branding-header.tsx
@@ -10,21 +10,28 @@ interface BrandingHeaderProps {
   onPublish: () => void
 }
 
-export const BrandingHeader = ({ cnameRecord, hasChanges, onPreview, onRevert, onPublish }: BrandingHeaderProps) => (
-  <div className="flex items-center gap-5 w-full">
-    <Button onClick={onPreview} type="button" variant="secondary" icon={<Eye size={16} />}>
-      Preview
-    </Button>
-    {hasChanges && (
-      <Button onClick={onRevert} type="button" variant="secondary" icon={<RotateCcw size={16} />}>
-        Revert Changes
+export const BrandingHeader = ({ cnameRecord, hasChanges, onPreview, onRevert, onPublish }: BrandingHeaderProps) => {
+  const prependHttps = (value?: string) => {
+    if (!value) return ''
+    return value.startsWith('http://') || value.startsWith('https://') ? value : `https://${value}`
+  }
+  const url = prependHttps(cnameRecord || '')
+  return (
+    <div className="flex items-center gap-5 w-full">
+      <Button onClick={onPreview} type="button" variant="secondary" icon={<Eye size={16} />}>
+        Preview
       </Button>
-    )}
-    <div className="flex items-center gap-10 flex-1">
-      <UrlInput disabled hasCopyButton placeholder={cnameRecord ?? 'Preview URL not available yet'} value={cnameRecord ?? ''} className="h-8" />
-      <Button className="ml-auto" variant="primary" icon={<BookUp size={16} />} onClick={onPublish}>
-        Publish
-      </Button>
+      {hasChanges && (
+        <Button onClick={onRevert} type="button" variant="secondary" icon={<RotateCcw size={16} />}>
+          Revert Changes
+        </Button>
+      )}
+      <div className="flex items-center gap-10 flex-1">
+        <UrlInput disabled hasCopyButton placeholder={cnameRecord ?? 'Preview URL not available yet'} value={url} className="h-8" />
+        <Button className="ml-auto" variant="primary" icon={<BookUp size={16} />} onClick={onPublish}>
+          Publish
+        </Button>
+      </div>
     </div>
-  </div>
-)
+  )
+}
