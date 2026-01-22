b95af960d32530afc7ed7cbfe9819cc05983a32b
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2026-01-21 23:22:38 -0700

    wip: add iframe
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

---
+++ apps/console/src/components/pages/protected/trust-center/analytics/analytics-page.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+export default function AnalyticsPage() {
+  return <iframe src="https://openlane.pirsch.io/?access=zdmnjumaAk8C3Ir5iaab&ui=hide&mode=dark" width="100%" height="100%" style={{ border: 'none' }}></iframe>
+}
