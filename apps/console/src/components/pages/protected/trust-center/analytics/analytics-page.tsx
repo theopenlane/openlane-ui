85fc719150c08cd8272c6a5f779283c577a67bc0
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2026-01-30 14:38:20 -0700

    feat: add pirsch access link instead of env var (#1371)
    * feat: add pirsch access link instead of env var
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    
    * cleannup
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- apps/console/src/components/pages/protected/trust-center/analytics/analytics-page.tsx
+++ apps/console/src/components/pages/protected/trust-center/analytics/analytics-page.tsx
@@ -1,23 +1,23 @@
 'use client'
 
 import React, { useContext, useEffect } from 'react'
-import { Globe, Plus } from 'lucide-react'
+import { Globe, Plus, Info } from 'lucide-react'
 import { useTheme } from 'next-themes'
-// import { useGetTrustCenter } from '@/lib/graphql-hooks/trust-center'
+import { useGetTrustCenter } from '@/lib/graphql-hooks/trust-center'
 import { Button } from '@repo/ui/button'
 import Link from 'next/link'
 import { BreadcrumbContext } from '@/providers/BreadcrumbContext'
+import { SUPPORT_EMAIL } from '@/constants'
 
 export default function AnalyticsPage() {
   const { resolvedTheme } = useTheme()
 
   //todo: connect this once we have a trust center pirsch access link and remove env variable
-  // const { data: trustCenterData } = useGetTrustCenter()
-  // const trustCenter = trustCenterData?.trustCenters?.edges?.[0]?.node
-  // const accessLink = trustCenter?.pirschAccessLink
-  // || process.env.NEXT_PUBLIC_ANALYTICS_ACCESS_LINK || ''
+  const { data: trustCenterData } = useGetTrustCenter()
+  const trustCenter = trustCenterData?.trustCenters?.edges?.[0]?.node
+  const accessLink = trustCenter?.pirschAccessLink
+  const customDomain = trustCenter?.customDomain?.cnameRecord
 
-  const accessLink = process.env.NEXT_PUBLIC_ANALYTICS_ACCESS_LINK
   const mode = resolvedTheme === 'dark' ? 'dark' : 'light'
 
   const { setCrumbs } = useContext(BreadcrumbContext)
@@ -25,7 +25,7 @@ export default function AnalyticsPage() {
   useEffect(() => {
     setCrumbs([{ label: 'Home', href: '/dashboard' }, { label: 'Trust Center' }, { label: 'Analytics', href: '/trust-center/analytics' }])
   }, [setCrumbs])
-  if (!accessLink) {
+  if (!accessLink && !customDomain) {
     return (
       <div className="relative w-full h-[600px]">
         <div className="absolute inset-0 border-2 border-dashed rounded-lg flex flex-col items-center justify-center text-center p-8 ">
@@ -35,6 +35,23 @@ export default function AnalyticsPage() {
           <Link href={'/trust-center/domain'}>
             <Button icon={<Plus size={16} />} iconPosition="left">
               Add Custom Domain
+            </Button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+
+  if (!accessLink && customDomain) {
+    return (
+      <div className="relative w-full h-[600px]">
+        <div className="absolute inset-0 border-2 border-dashed rounded-lg flex flex-col items-center justify-center text-center p-8 ">
+          <Globe size={24} className="mb-4 text-muted-foreground" />
+          <h3 className="text-sm font-medium mb-1">Unlock analytics</h3>
+          <p className="text-sm text-muted-foreground mb-6">Analytics is not currently available for you domain. If this continues, please reach out to support.</p>
+          <Link href={SUPPORT_EMAIL}>
+            <Button icon={<Info size={16} />} iconPosition="left">
+              Contact Support
             </Button>
           </Link>
         </div>
