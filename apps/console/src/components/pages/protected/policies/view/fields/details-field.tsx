f7def0112701bad5700677345df193d38bb32271
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2026-02-03 14:51:20 -0700

    put label back
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- apps/console/src/components/pages/protected/policies/view/fields/details-field.tsx
+++ apps/console/src/components/pages/protected/policies/view/fields/details-field.tsx
@@ -24,6 +24,7 @@ const DetailsField: React.FC<TDetailsFieldProps> = ({ isEditing, form, policy, d
   return isEditing ? (
     <div className="w-full relative">
       <label htmlFor="policy" className="block text-sm font-medium text-muted-foreground mb-1">
+        Policy
       </label>
       <Controller
         control={form.control}
