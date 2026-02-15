9df59ea8c1cd6c8d557305bb72b95f164c1725bb
bruno papista <bruno.papista@gmail.com>
2026-02-15 01:44:59 +0100

    removing unused files and routes

--- apps/console/src/app/(protected)/entities/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { PageHeading } from '@repo/ui/page-heading'
-
-const Page: React.FC = () => {
-  return <PageHeading heading="Entities" />
-}
-
-export default Page
--- apps/console/src/components/shared/lib/strings.tsx
+++
@@ -1,9 +0,0 @@
-export function toTitleCase(str: string) {
-  return str
-    .toLowerCase()
-    .split(' ')
-    .map((word: string) => {
-      return word.charAt(0).toUpperCase() + word.slice(1)
-    })
-    .join(' ')
-}
