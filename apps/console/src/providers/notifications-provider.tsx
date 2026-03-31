98f53d48191c50f30e91b7862ece8e4c208b5365
BrunoKucevic <42520577+BrunoKucevic@users.noreply.github.com>
2026-03-31 16:49:13 +0200

    ISS-1905: Real-Time Notification Alert (#1644)
    * ISS-1905: Real-Time Notification Alert
    
    * work
    
    Signed-off-by: Bruno Kucevic <brunokucevic@gmail.com>
    
    * pr fix
    
    Signed-off-by: Bruno Kucevic <brunokucevic@gmail.com>
    
    ---------
    
    Signed-off-by: Bruno Kucevic <brunokucevic@gmail.com>

---
+++ apps/console/src/providers/notifications-provider.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import React, { createContext, use, useEffect, useRef } from 'react'
+import { useWebsocketNotifications, type Notification } from '@/lib/graphql-hooks/websocket/use-websocket-notifications'
+
+type NewNotificationListener = (notification: Notification) => void
+
+type NotificationsContextValue = ReturnType<typeof useWebsocketNotifications> & {
+  addNewNotificationListener: (listener: NewNotificationListener) => () => void
+}
+
+const NotificationsContext = createContext<NotificationsContextValue | null>(null)
+
+export const NotificationsProvider = ({ children }: { children: React.ReactNode }) => {
+  const websocketNotifications = useWebsocketNotifications()
+  const { notifications, subscriptionStartedAt } = websocketNotifications
+  const listenersRef = useRef<NewNotificationListener[]>([])
+  const seenIdsRef = useRef<Set<string>>(new Set())
+
+  useEffect(() => {
+    notifications.forEach((n) => {
+      if (!seenIdsRef.current.has(n.id)) {
+        seenIdsRef.current.add(n.id)
+        const isNew = subscriptionStartedAt !== null && n.createdAt != null && new Date(n.createdAt).getTime() >= subscriptionStartedAt
+        if (isNew) {
+          listenersRef.current.forEach((listener) => listener(n))
+        }
+      }
+    })
+  }, [notifications, subscriptionStartedAt])
+
+  const addNewNotificationListener = (listener: NewNotificationListener) => {
+    listenersRef.current.push(listener)
+    return () => {
+      listenersRef.current = listenersRef.current.filter((l) => l !== listener)
+    }
+  }
+
+  return <NotificationsContext value={{ ...websocketNotifications, addNewNotificationListener }}>{children}</NotificationsContext>
+}
+
+export const useNotificationsContext = () => {
+  const ctx = use(NotificationsContext)
+  if (!ctx) throw new Error('useNotificationsContext must be used within NotificationsProvider')
+  return ctx
+}
