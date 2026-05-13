9fc273931f1259f3d224b9005fb4938a8cc00f3b
papistacoding <bruno.papista@gmail.com>
2026-05-13 19:52:43 +0200

    remove custom colors

--- packages/ui/src/components/ui/font-color-toolbar-button.tsx
+++ packages/ui/src/components/ui/font-color-toolbar-button.tsx
@@ -4,9 +4,7 @@ import React from 'react'
 
 import type { DropdownMenuItemProps, DropdownMenuProps } from '@radix-ui/react-dropdown-menu'
 
-import { useComposedRef } from 'platejs/react'
-import debounce from 'lodash/debounce.js'
-import { EraserIcon, PlusIcon } from 'lucide-react'
+import { EraserIcon } from 'lucide-react'
 import { KEYS } from 'platejs'
 import { useEditorRef, useEditorSelector } from 'platejs/react'
 
@@ -51,16 +49,9 @@ export function FontColorToolbarButton({
 
         editor.tf.addMarks({ [nodeType]: value })
       }
-    },
-    [editor, nodeType],
-  )
-
-  const updateColorAndClose = React.useCallback(
-    (value: string) => {
-      updateColor(value)
       onToggle()
     },
-    [onToggle, updateColor],
+    [editor, nodeType, onToggle],
   )
 
   const clearColor = React.useCallback(() => {
@@ -96,14 +87,7 @@ export function FontColorToolbarButton({
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="start">
-        <ColorPicker
-          color={selectedColor || color}
-          clearColor={clearColor}
-          colors={getPaletteForNodeType(nodeType)}
-          customColors={[]}
-          updateColor={updateColorAndClose}
-          updateCustomColor={updateColor}
-        />
+        <ColorPicker color={selectedColor || color} clearColor={clearColor} colors={getPaletteForNodeType(nodeType)} updateColor={updateColor} />
       </DropdownMenuContent>
     </DropdownMenu>
   )
@@ -114,26 +98,17 @@ function PureColorPicker({
   clearColor,
   color,
   colors,
-  customColors,
   updateColor,
-  updateCustomColor,
   ...props
 }: React.ComponentProps<'div'> & {
   colors: TColor[]
-  customColors: TColor[]
   clearColor: () => void
   updateColor: (color: string) => void
-  updateCustomColor: (color: string) => void
   color?: string
 }) {
   return (
     <div className={cn('flex flex-col', className)} {...props}>
-      <ToolbarMenuGroup label="Custom Colors">
-        <ColorCustom color={color} className="px-2" colors={colors} customColors={customColors} updateColor={updateColor} updateCustomColor={updateCustomColor} />
-      </ToolbarMenuGroup>
-      <ToolbarMenuGroup label="Default Colors">
-        <ColorDropdownMenuItems color={color} className="px-2" colors={colors} updateColor={updateColor} />
-      </ToolbarMenuGroup>
+      <ColorDropdownMenuItems color={color} className="p-2" colors={colors} updateColor={updateColor} />
       {color && (
         <ToolbarMenuGroup>
           <DropdownMenuItem className="p-2" onClick={clearColor}>
@@ -146,104 +121,7 @@ function PureColorPicker({
   )
 }
 
-const ColorPicker = React.memo(PureColorPicker, (prev, next) => prev.color === next.color && prev.colors === next.colors && prev.customColors === next.customColors)
-
-function ColorCustom({
-  className,
-  color,
-  colors,
-  customColors,
-  updateColor,
-  updateCustomColor,
-  ...props
-}: {
-  colors: TColor[]
-  customColors: TColor[]
-  updateColor: (color: string) => void
-  updateCustomColor: (color: string) => void
-  color?: string
-} & React.ComponentPropsWithoutRef<'div'>) {
-  const [customColor, setCustomColor] = React.useState<string>()
-  const [value, setValue] = React.useState<string>(color || '#000000')
-
-  React.useEffect(() => {
-    if (!color || customColors.some((c) => c.value === color) || colors.some((c) => c.value === color)) {
-      return
-    }
-
-    setCustomColor(color)
-  }, [color, colors, customColors])
-
-  const computedColors = React.useMemo(
-    () =>
-      customColor
-        ? [
-            ...customColors,
-            {
-              isBrightColor: false,
-              name: '',
-              value: customColor,
-            },
-          ]
-        : customColors,
-    [customColor, customColors],
-  )
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const updateCustomColorDebounced = React.useCallback(debounce(updateCustomColor, 100), [updateCustomColor])
-
-  return (
-    <div className={cn('relative flex flex-col gap-4', className)} {...props}>
-      <ColorDropdownMenuItems color={color} colors={computedColors} updateColor={updateColor}>
-        <ColorInput
-          value={value}
-          onChange={(e) => {
-            setValue(e.target.value)
-            updateCustomColorDebounced(e.target.value)
-          }}
-        >
-          <DropdownMenuItem
-            className={cn(
-              buttonVariants({
-                size: 'icon',
-                variant: 'outline',
-              }),
-              'absolute top-1 right-2 bottom-2 flex size-8 items-center justify-center rounded-full',
-            )}
-            onSelect={(e) => {
-              e.preventDefault()
-            }}
-          >
-            <span className="sr-only">Custom</span>
-            <PlusIcon />
-          </DropdownMenuItem>
-        </ColorInput>
-      </ColorDropdownMenuItems>
-    </div>
-  )
-}
-
-function ColorInput({ children, className, value = '#000000', ...props }: React.ComponentProps<'input'>) {
-  const inputRef = React.useRef<HTMLInputElement | null>(null)
-
-  return (
-    <div className="flex flex-col items-center">
-      {React.Children.map(children, (child) => {
-        if (!child) return child
-
-        return React.cloneElement(
-          child as React.ReactElement<{
-            onClick: () => void
-          }>,
-          {
-            onClick: () => inputRef.current?.click(),
-          },
-        )
-      })}
-      <input {...props} ref={useComposedRef(props.ref, inputRef)} className={cn('size-0 overflow-hidden border-0 p-0', className)} value={value} type="color" />
-    </div>
-  )
-}
+const ColorPicker = React.memo(PureColorPicker, (prev, next) => prev.color === next.color && prev.colors === next.colors)
 
 type TColor = {
   isBrightColor: boolean
@@ -310,7 +188,7 @@ export function ColorDropdownMenuItems({
   color?: string
 } & React.ComponentProps<'div'>) {
   return (
-    <div className={cn('grid grid-cols-[repeat(10,1fr)] place-items-center gap-x-1', className)} {...props}>
+    <div className={cn('grid grid-cols-9 place-items-center gap-x-1', className)} {...props}>
       <TooltipProvider>
         {colors.map(({ isBrightColor, name, value }) => (
           <ColorDropdownMenuItem name={name} key={name ?? value} value={value} isBrightColor={isBrightColor} isSelected={color === value} updateColor={updateColor} />
