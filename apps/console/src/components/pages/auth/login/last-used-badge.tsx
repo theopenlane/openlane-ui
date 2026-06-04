42e1497450eadf0a4fb623f7769bf91ce2d46bb7
papistacoding <bruno.papista@gmail.com>
2026-06-04 17:16:33 +0200

    blue 50% opacity

--- apps/console/src/components/pages/auth/login/last-used-badge.tsx
+++ apps/console/src/components/pages/auth/login/last-used-badge.tsx
@@ -13,7 +13,10 @@ export const LastUsedBadge = ({ provider, lastUsedProvider, floating = false, cl
   if (lastUsedProvider !== provider) return null
 
   return (
-    <Badge variant="primary" className={cn('bg-primary/80 text-white', floating ? 'absolute -top-3.5 left-1/2 -translate-x-1/2 z-10 w-fit whitespace-nowrap' : 'self-center', className)}>
+    <Badge
+      variant="primary"
+      className={cn('bg-blue-500/50 text-white border-transparent', floating ? 'absolute -top-3.5 left-1/2 -translate-x-1/2 z-10 w-fit whitespace-nowrap' : 'self-center', className)}
+    >
       Last used
     </Badge>
   )
