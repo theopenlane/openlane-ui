2b1cbbdc447b0a7826f1c4da9ac2820d85181a16
Bruno Papista <78353799+papistacoding@users.noreply.github.com>
2026-07-09 06:22:12 +0200

    ISS-2398: Double Filters when clicking Chart (#1992)

--- apps/console/src/components/pages/protected/findings/table/page.tsx
+++ apps/console/src/components/pages/protected/findings/table/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React from 'react'
 import { bulkEditFieldSchema } from '../hooks/use-form-schema'
 import { useCreateBulkCSVFinding, useBulkEditFinding, useBulkDeleteFinding } from '@/lib/graphql-hooks/finding'
 import { useSearchParams, useRouter } from 'next/navigation'
@@ -9,7 +9,7 @@ import { breadcrumbs, getFilterFields, visibilityFields } from './table-config'
 import { type FindingTablePageConfig, objectType, objectName, tableKey, exportType, orderFieldEnum, defaultSorting } from './types'
 import { getColumns } from './columns'
 import TableComponent from './table'
-import { type UpdateFindingInput, FindingSecurityLevel } from '@repo/codegen/src/schema'
+import { type UpdateFindingInput } from '@repo/codegen/src/schema'
 import { useFindingSheetConfig } from '../hooks/use-finding-sheet-config'
 import TaskDetailsSheet from '../../tasks/create-task/sidebar/task-details-sheet'
 import ViewFindingSheet from '../view-finding-sheet'
@@ -22,7 +22,6 @@ const FindingPage: React.FC = () => {
   const searchParams = useSearchParams()
   const id = searchParams.get('id')
   const isCreate = searchParams.get('create') === 'true'
-  const [selectedSeverity, setSelectedSeverity] = useState<'critical' | 'high' | 'medium' | 'low' | null>(null)
 
   const { enumOpts, form, ...sheetConfig } = useFindingSheetConfig(null, isCreate)
 
@@ -41,8 +40,6 @@ const FindingPage: React.FC = () => {
     isPending: baseBulkCreateMutation.isPending,
     mutateAsync: async (params: { input: File }) => baseBulkCreateMutation.mutateAsync({ input: params.input }),
   }
-
-  const severityWhereFilter = selectedSeverity ? { securityLevelIn: [FindingSecurityLevel[selectedSeverity.toUpperCase() as keyof typeof FindingSecurityLevel]], open: true } : undefined
 
   const tableConfig: FindingTablePageConfig = {
     objectType,
@@ -71,11 +68,10 @@ const FindingPage: React.FC = () => {
     },
     bulkEditFormSchema: bulkEditFieldSchema,
     enumOpts,
-    additionalWhereFilter: severityWhereFilter,
     defaultFilterValues: DEFAULT_FILTER_VALUES,
     beforeTable: (
       <>
-        <FindingSeverityChart selectedSeverity={selectedSeverity} onSeveritySelect={setSelectedSeverity} />
+        <FindingSeverityChart tableKey={tableKey} />
       </>
     ),
   }
