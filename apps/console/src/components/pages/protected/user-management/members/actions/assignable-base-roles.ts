6f5716bf185a40fb1741be047a864e50fbb281bb
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2026-08-07 14:01:44 -0600

    add assignment to auditor role
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- apps/console/src/components/pages/protected/user-management/members/actions/assignable-base-roles.ts
+++ apps/console/src/components/pages/protected/user-management/members/actions/assignable-base-roles.ts
@@ -1,5 +1,3 @@
 import { OrgMembershipRole } from '@repo/codegen/src/schema'
 
-export const ASSIGNABLE_BASE_ROLES: OrgMembershipRole[] = Object.values(OrgMembershipRole).filter(
-  (role) => role !== OrgMembershipRole.OWNER && !role.includes('USER') && role !== OrgMembershipRole.AUDITOR,
-)
+export const ASSIGNABLE_BASE_ROLES: OrgMembershipRole[] = Object.values(OrgMembershipRole).filter((role) => role !== OrgMembershipRole.OWNER && !role.includes('USER'))
