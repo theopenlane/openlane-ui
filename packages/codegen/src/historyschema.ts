2b5475a4f0651012e848e2ba952312fee3353424
papistacoding <bruno.papista@gmail.com>
2026-08-14 18:40:46 +0200

    regenerate codegen schema with hero image types
    Signed-off-by: papistacoding <bruno.papista@gmail.com>

--- packages/codegen/src/historyschema.ts
+++ packages/codegen/src/historyschema.ts
@@ -9822,6 +9822,7 @@ export interface GetTrustCenterQuery {
           ndaApproverGroupID: string | null
           logoFile: { id: string; base64: string | null } | null
           faviconFile: { id: string; base64: string | null } | null
+          heroImageFile: { id: string; base64: string | null } | null
           ndaApproverGroup: { id: string; displayName: string; name: string } | null
         } | null
         previewSetting: {
@@ -9846,6 +9847,7 @@ export interface GetTrustCenterQuery {
           updatedAt: any
           logoFile: { id: string; base64: string | null } | null
           faviconFile: { id: string; base64: string | null } | null
+          heroImageFile: { id: string; base64: string | null } | null
         } | null
         watermarkConfig: {
           id: string
@@ -9867,10 +9869,20 @@ export type UpdateTrustCenterSettingMutationVariables = Exact<{
   input: Types.UpdateTrustCenterSettingInput
   faviconFile?: any
   logoFile?: any
+  heroImageFile?: any
 }>
 
 export interface UpdateTrustCenterSettingMutation {
-  updateTrustCenterSetting: { trustCenterSetting: { id: string; logoRemoteURL: string | null; faviconRemoteURL: string | null; faviconFile: { id: string } | null; logoFile: { id: string } | null } }
+  updateTrustCenterSetting: {
+    trustCenterSetting: {
+      id: string
+      logoRemoteURL: string | null
+      faviconRemoteURL: string | null
+      faviconFile: { id: string } | null
+      logoFile: { id: string } | null
+      heroImageFile: { id: string } | null
+    }
+  }
 }
 
 export type CreateCustomDomainMutationVariables = Exact<{
--- packages/codegen/src/schema.ts
+++ packages/codegen/src/schema.ts
@@ -9781,6 +9781,7 @@ export interface GetTrustCenterQuery {
           ndaApproverGroupID: string | null
           logoFile: { id: string; base64: string | null } | null
           faviconFile: { id: string; base64: string | null } | null
+          heroImageFile: { id: string; base64: string | null } | null
           ndaApproverGroup: { id: string; displayName: string; name: string } | null
         } | null
         previewSetting: {
@@ -9805,6 +9806,7 @@ export interface GetTrustCenterQuery {
           updatedAt: any
           logoFile: { id: string; base64: string | null } | null
           faviconFile: { id: string; base64: string | null } | null
+          heroImageFile: { id: string; base64: string | null } | null
         } | null
         watermarkConfig: {
           id: string
@@ -9826,10 +9828,20 @@ export type UpdateTrustCenterSettingMutationVariables = Exact<{
   input: Types.UpdateTrustCenterSettingInput
   faviconFile?: any
   logoFile?: any
+  heroImageFile?: any
 }>
 
 export interface UpdateTrustCenterSettingMutation {
-  updateTrustCenterSetting: { trustCenterSetting: { id: string; logoRemoteURL: string | null; faviconRemoteURL: string | null; faviconFile: { id: string } | null; logoFile: { id: string } | null } }
+  updateTrustCenterSetting: {
+    trustCenterSetting: {
+      id: string
+      logoRemoteURL: string | null
+      faviconRemoteURL: string | null
+      faviconFile: { id: string } | null
+      logoFile: { id: string } | null
+      heroImageFile: { id: string } | null
+    }
+  }
 }
 
 export type CreateCustomDomainMutationVariables = Exact<{
