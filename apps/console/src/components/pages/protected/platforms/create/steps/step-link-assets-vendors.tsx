a62ebb7ac0aa2eb6c9330e359d52523b70a00759
Bruno Papista <78353799+papistacoding@users.noreply.github.com>
2026-09-09 19:31:48 +0200

    ISS-2861: Date Range Filter Field (#2277)
    Signed-off-by: papistacoding <bpapista@theopenlane.io>

--- apps/console/src/components/pages/protected/platforms/create/steps/step-link-assets-vendors.tsx
+++ apps/console/src/components/pages/protected/platforms/create/steps/step-link-assets-vendors.tsx
@@ -134,7 +134,7 @@ const MultiSelectField: React.FC<MultiSelectProps> = ({ fieldName, oppositeField
                                 <TooltipTrigger asChild>
                                   <div>{row}</div>
                                 </TooltipTrigger>
-                                <TooltipContent side="top" align="center" sideOffset={-4}>
+                                <TooltipContent portal side="top" align="center" sideOffset={-4}>
                                   {disabledReason ?? 'Already selected in the opposite list'}
                                 </TooltipContent>
                               </Tooltip>
